8397bee9a914b308b451d6dc3a2c82bd49a98ba1
rlratzel <rlratzel@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-14 22:21:11 +0000

    * Making enthought parent directory a package.

---
+++ enthought/__init__.py
@@ -0,0 +1,30 @@
+#------------------------------------------------------------------------------
+# Copyright 2003, Enthought, Inc.
+# All rights reserved.
+# 
+# This software is provided without warranty under the terms of the BSD
+# license included in enthought/LICENSE.txt and may be redistributed only
+# under the conditions described in the aforementioned license.  The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+# Thanks for using Enthought open source!
+# 
+# Author: Enthought, Inc.
+# Description: <Enthought library component>
+#------------------------------------------------------------------------------
+"""
+==========================
+Enthought Library
+========================== 
+
+The Enthought Open Source Library
+
+Copyright 2002-2006, Enthought, Inc.
+
+
+"""
+
+try:
+    # if the code is ran from an egg, the namespace must be declared
+    __import__('pkg_resources').declare_namespace(__name__)
+except:
+    pass
