f4f5a3fb33c0281f7f931c23e88f01fb87083900
rlratzel <rlratzel@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-14 22:20:43 +0000

    * Copying new setuptools-based setup.py from enthought_split branch.

---
+++ setup.py
@@ -0,0 +1,37 @@
+import setuptools
+from numpy.distutils.core import setup
+
+
+def configuration(parent_package='', top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration(None, parent_package, top_path)
+    config.set_options(
+        ignore_setup_xxx_py=True,
+        assume_default_configuration=True,
+        delegate_options_to_subpackages=True,
+        quiet=True,
+    )
+
+    config.add_subpackage('enthought.tvtk')
+    config.add_data_files('enthought/__init__.py')
+
+    return config
+
+
+setup(
+    name = 'enthought.tvtk',
+    version      = '1.0.0',
+    description  = "Traited VTK",
+    author       = "Prabhu Ramachandran",
+    author_email = "prabhu_r@users.sf.net",
+    url          = 'http://www.enthought.com/enthought/wiki/TVTK',
+    license = "BSD",
+    install_requires = [
+        'VTK',
+        'enthought.pyface[tvtk]',  # fix? circular dependency
+    ],
+    namespace_packages = [
+        "enthought",
+    ],
+    **configuration().todict()
+)
