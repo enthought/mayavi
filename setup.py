f44d4d730892f10004646666a71f6933316b771f
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-06-12 12:35:20 +0000

    * Refactored setup.py's to enable release and dev builds to work with the same set of declared packages and version numbers. * Renamed ets as version 2.5 to ensure its higher than the 2.4 version already available from the egg repo.

--- setup.py
+++ setup.py
@@ -18,6 +18,19 @@ def configuration(parent_package='', top_path=None):
     return config
 
 
+# Function to convert simple ETS component names and versions to a requirements
+# spec that works for both development builds and stable builds.
+def gendeps(list):
+    return ['%s >=%s.dev, <=%s.dev' % (p,min,max) for p,min,max in list]
+
+# Declare our installation requirements.
+install_requires = gendeps([
+    ("enthought.pyface", "2.0b1", "3"),
+    ("enthought.persistence", "2.0b1", "3"),
+    ])
+print 'install_requires:\n\t%s' % '\n\t'.join(install_requires)
+
+
 setup(
     name = 'enthought.tvtk',
     version      = '2.0b1',
@@ -26,11 +39,14 @@ setup(
     author_email = "prabhu_r@users.sf.net",
     url          = 'http://www.enthought.com/enthought/wiki/TVTK',
     license = "BSD",
-    install_requires = [
-        # 'VTK',  # fixme: VTK is not available as an egg on all platforms.
-        'enthought.pyface >=2.0b1, <3',
-        'enthought.persistence >=2.0b1, <3',
-    ],
+    install_requires = install_requires,
+    extras_require = {
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+            # 'VTK',  # fixme: VTK is not available as an egg on all platforms.
+            ],
+    },
     namespace_packages = [
         "enthought",
     ],
