7605246db4891213f2e8eaaf7cb43f1950ba9859
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-09 12:52:58 +0000

    * Making the patch level explicit in the version specifier.

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
         ],
     test_suite = 'nose.collector',
     url = 'http://code.enthought.com/tvtk/',
-    version = '2.0b2',
+    version = '2.0.0b2',
     zip_safe = False,
     **configuration().todict()
     )
