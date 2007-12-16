0c5de009c0ab7219dbf45d4a7b0fe4c7efc4a74d
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-12-16 06:57:13 +0000

    BUG: The test was incorrect since it was relying on 'l' being the right typecode for ID arrays.

--- enthought/tvtk/tests/test_array_ext.py
+++ enthought/tvtk/tests/test_array_ext.py
@@ -12,12 +12,13 @@ import vtk
 import weakref
 import gc
 
+from enthought.tvtk.array_handler import ID_TYPE_CODE
 from enthought.tvtk.array_ext import set_id_type_array
 
 class TestArrayExt(unittest.TestCase):
     def test_set_id_type_array(self):
         N = 5
-        a = numpy.zeros((N,4), 'l')
+        a = numpy.zeros((N,4), ID_TYPE_CODE)
         a[:,1] = 1
         a[:,2] = 2
         a[:,3] = 3
@@ -26,28 +27,28 @@ class TestArrayExt(unittest.TestCase):
             return numpy.sum(numpy.ravel(x) - numpy.ravel(y[:,1:]))                
 
         # Test contiguous arrays.
-        b = numpy.zeros((N,5), 'l')
+        b = numpy.zeros((N,5), ID_TYPE_CODE)
         set_id_type_array(a, b)
         self.assertEqual(diff_arr(a, b), 0)
 
         # Test non-contiguous arrays.
-        b = numpy.zeros((N,3), 'l')
+        b = numpy.zeros((N,3), ID_TYPE_CODE)
         set_id_type_array(a[:,::2], b)
         self.assertEqual(diff_arr(a[:,::2], b), 0)
 
         # Test 1D array.
-        b = numpy.zeros(N*5, 'l')
+        b = numpy.zeros(N*5, ID_TYPE_CODE)
         set_id_type_array(a, b)
         self.assertEqual(diff_arr(a, numpy.reshape(b, (N,5))), 0)
 
         # Test assertions.
         d = a.astype('d')
-        b = numpy.zeros((N, 5), 'l')
+        b = numpy.zeros((N, 5), ID_TYPE_CODE)
         self.assertRaises(AssertionError, set_id_type_array,
                           d, b)
         
         # B should b contiguous.
-        b = numpy.zeros((N, 10), 'l')
+        b = numpy.zeros((N, 10), ID_TYPE_CODE)
         self.assertRaises(AssertionError, set_id_type_array,
                           a, b[:,::2])
 
@@ -55,11 +56,11 @@ class TestArrayExt(unittest.TestCase):
                           a[0], b)
 
         # Test size check assertion.
-        b = numpy.zeros((N, 4), 'l')
+        b = numpy.zeros((N, 4), ID_TYPE_CODE)
         self.assertRaises(AssertionError, set_id_type_array,
                           a, b)
         
-        b = numpy.zeros(N*6, 'l')
+        b = numpy.zeros(N*6, ID_TYPE_CODE)
         self.assertRaises(AssertionError, set_id_type_array,
                           a, b)
 
