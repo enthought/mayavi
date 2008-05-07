a9c75b32f92ffb9571347522db747573a3fc9418
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-05-07 10:55:06 +0000

    REORG: Removing older plugins directory.

--- enthought/tvtk/plugins/browser/services.py
+++
@@ -1,6 +0,0 @@
-# Author: Prabhu Ramachandran <prabhu_r@users.sf.net>
-# Copyright (c) 2006, Enthought, Inc.
-# License: BSD Style.
-
-# The ID's of the services we offer.
-ITVTKBROWSER = 'enthought.tvtk.plugins.browser.ITVTKBrowser'
--- enthought/tvtk/plugins/scene/services.py
+++
@@ -1,6 +0,0 @@
-# Author: Prabhu Ramachandran <prabhu_r@users.sf.net>
-# Copyright (c) 2006, Enthought, Inc.
-# License: BSD Style.
-
-#The ID's of the services we offer.
-ITVTKSCENE = 'enthought.tvtk.plugins.scene.ITVTKScene'
