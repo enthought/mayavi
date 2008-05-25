b3a0ca8557b55d9c3a7bde4b0004583503ba586e
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-05-25 13:13:37 +0000

    ENH: Updating api.py with newly added filters.

--- enthought/mayavi/filters/api.py
+++ enthought/mayavi/filters/api.py
@@ -1,11 +1,15 @@
 """ Defines the publicly accessible MayaVi2 filters.
 """
 
-# Author: Frederic Petit 
+# Authors: Frederic Petit, Prabhu Ramachandran and Gael Varoquaux
 # Copyright (c) 2007, Enthought, Inc. 
 # License: BSD Style.
 
+from cell_derivatives import CellDerivatives
 from cell_to_point_data import CellToPointData
+from collection import Collection
+from contour import Contour
+from cut_plane import CutPlane
 from decimatepro import DecimatePro
 from delaunay2d import Delaunay2D
 from delaunay3d import Delaunay3D
@@ -14,18 +18,24 @@ from extract_edges import ExtractEdges
 from extract_grid import ExtractGrid
 from extract_tensor_components import ExtractTensorComponents
 from extract_unstructured_grid import ExtractUnstructuredGrid
-from extract_vector_norm import ExtractVectorNorm
 from extract_vector_components import ExtractVectorComponents
+from extract_vector_norm import ExtractVectorNorm
 from gaussian_splatter import GaussianSplatter
 from greedy_terrain_decimation import GreedyTerrainDecimation
 from mask_points import MaskPoints
+from optional import Optional
 from point_to_cell_data import PointToCellData
 from poly_data_normals import PolyDataNormals
 from quadric_decimation import QuadricDecimation
 from select_output import SelectOutput
+from set_active_attribute import SetActiveAttribute
 from threshold import Threshold
 from transform_data import TransformData
 from triangle_filter import TriangleFilter
 from tube import Tube
+from user_defined import UserDefined
+from vorticity import Vorticity
 from warp_scalar import WarpScalar
 from warp_vector import WarpVector
+from wrapper import Wrapper
+
