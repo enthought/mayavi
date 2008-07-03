1aec583a52ac9edc95138f5df356da60451dfe2b
vibha <vibha@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-03 20:58:31 +0000

    Add handler to the view.

--- enthought/tvtk/view/parametric_function_source_view.py
+++ enthought/tvtk/view/parametric_function_source_view.py
@@ -1,10 +1,13 @@
 from enthought.traits.ui.api import View, HGroup, Item
+from enthought.tvtk.tvtk_base import TVTKBaseHandler
     
 view = View((['generate_texture_coordinates'], ['scalar_mode'],
     HGroup(Item('u_resolution', label = 'u'), 
           Item('v_resolution', label = 'v'),
           Item('w_resolution', label = 'w'),
           label = 'Resolution', show_border = True)),
+    Item('handler.advanced_view'),
+    handler = TVTKBaseHandler,
     title='Edit ParametricFunctionSource properties', scrollable=True,
     buttons=['OK', 'Cancel'])
     
