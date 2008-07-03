f5c5c565306ab20557932ea40a02e7a6b15c7144
judah <judah@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-03 17:42:48 +0000

    Removed tabs, and turned them into buttons.  Reorganized layout, and fixed items that should have had enabled_when on.

--- enthought/mayavi/view/lut_manager_view.py
+++ enthought/mayavi/view/lut_manager_view.py
@@ -1,4 +1,4 @@
-from enthought.traits.ui.api import Item, Group, View, ImageEnumEditor
+from enthought.traits.ui.api import Item, Group, View, ImageEnumEditor, InstanceEditor
 from enthought.mayavi.core import lut 
 from enthought.mayavi.core.lut_manager import lut_mode_list
 import os
@@ -6,33 +6,53 @@ lut_image_dir = os.path.dirname(lut.__file__)
 
 
 
-# The view of this object.
+# The view of the LUT Manager object.
 view = View(Group(Item(name='lut_mode',
-                   editor=ImageEnumEditor( values=lut_mode_list() , 
-                                            cols=6,
-                                            path=lut_image_dir)),
+                       editor=ImageEnumEditor(values=lut_mode_list(), 
+                                              cols=6,
+                                              path=lut_image_dir)),
                   Item(name='file_name', visible_when="lut_mode=='file'"), 
-                  Item(name='show_scalar_bar'),
-                  Item(name='reverse_lut'),
                   Item(name='number_of_colors'),
-                  Item(name='number_of_labels'),
-                  Item(name='shadow'),
-                  Item(name='use_default_name'),
-                  Item(name='data_name'),
-                  Item(name='use_default_range'),
-                  Item(name='data_range',
-                       enabled_when='not object.use_default_range'),
-                  label='LUT Manager',
+                  Item(name='reverse_lut'),
+                  
+                  Group(Item(name='show_scalar_bar'),
+                      Group(   
+                          Item(name='number_of_labels'),
+                          Item(name='shadow'),
+                          Item(name='use_default_name'),
+                          Item(name='data_name',
+                               enabled_when='not object.use_default_name'),
+                          Item(name='use_default_range'),
+                          Item(name='data_range',
+                               enabled_when='not object.use_default_range'),
+                          Item(name='_title_text_property',
+                               show_label=False,
+                               editor=InstanceEditor(label='Edit bar Title')),
+                          Item(name='_label_text_property',
+                               show_label=False,
+                               editor=InstanceEditor(label='Edit bar Text'),
+                               label='Edit bar Text'),
+                          enabled_when='show_scalar_bar==True',
+                      ),    
+                      show_border=True,
                   ),
-            Group(Item(name='_title_text_property',
-                       style='custom',
-                       resizable=True),
-                  show_labels=False,
-                  label='Title'),
-            Group(Item(name='_label_text_property',
-                       style='custom',
-                       resizable=True),
-                  show_labels=False,
-                  label='Labels'),
-            resizable=True
-            )
+                       
+                  label='LUT (Look Up Table) Manager',
+             ),
+
+# Delete this once we're sure we want to keep the new integrated format.
+#            Group(Item(name='_title_text_property',
+#                       style='custom',
+#                       resizable=True),
+#                  show_labels=False,
+#                  defined_when='show_scalar_bar==True',
+#                  label='Title'),
+#            Group(Item(name='_label_text_property',
+#                       style='custom',
+#                       resizable=True),
+#                  enabled_when='show_scalar_bar==True',
+#                  show_labels=False,
+#                  label='Labels'),
+            
+            resizable=True,
+        )
