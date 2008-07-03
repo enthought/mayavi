63139c816da4c6fdf77db642e5be3353847eb76f
jwheeler <jwheeler@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-03 16:26:43 +0000

    Change doc commit script to use optparse

--- commit_docs.py
+++ commit_docs.py
@@ -1,53 +1,44 @@
 #!/usr/bin/env python
 # encoding: utf-8
-import getopt
+from optparse import OptionParser
 import os.path
 import shutil
-from subprocess import Popen, STDOUT, PIPE
-import sys
+from subprocess import Popen
 import tempfile
-
-help_message = '''
-Build and commit documentation to the CEC (code.enthought.com) www repos.
-
-This is for use by developers only, and depeneds on SVN being available from
-the command-line interface.
-
-Options:
-
-    -d/--docsrc     Set the path to the documentation source.
-
-    -h/--help       Print this help text.
-
-    -m/--message    Customize the commit message to the web server SVN.
-
-    -v/--verbose    Show all output from commands run internally.
-'''
 
 CEC_MAYAVI_SVN = 'https://svn.enthought.com/svn/internal/www/code.enthought' \
                      '.com/projects/mayavi/'
 
-def debug_print(msg, **options):
-    if options['stdout'] == None:
-        print msg % options
+def find_stdout(options):
+    if not hasattr(options, 'stdout'):
+        if options.verbose:
+            options.stdout = None
+        else:
+            options.stdout = open(os.devnull, 'w')
 
-def build_html(**options):
-    debug_print('Building html', **options)
+    return options.stdout
 
-    Popen('sphinx-build -b html %(docsrc)s %(temp)s/html/' % options,
-        stdout=options['stdout'], shell=True).wait()
+def debug_print(msg, options):
+    if options.verbose == None:
+        print msg
 
-def build_latex(**options):
-    debug_print('Building LaTeX', **options)
+def build_html(options):
+    debug_print('Building html', options)
+
+    Popen('sphinx-build -b html %s %s/html/' % (options.docsrc, options.temp),
+        stdout=find_stdout(options), shell=True).wait()
+
+def build_latex(options):
+    debug_print('Building LaTeX', options)
 
     # Build the LaTeX source files (ignore cwd property)
-    Popen('sphinx-build -b latex %(docsrc)s %(temp)s/latex/' % options,
-        shell=True, stdout=options['stdout']).wait()
+    Popen('sphinx-build -b latex %s %s/latex/' % (options.docsrc,
+        options.temp), stdout=find_stdout(options), shell=True).wait()
 
     kwargs = {
-        'cwd': '%(temp)s/latex/' % options,
+        'cwd': '%s/latex/' % options.temp,
         'shell': True,
-        'stdout': options['stdout'],
+        'stdout': find_stdout(options),
     }
 
     # Build LaTeX (stupidly difficult)
@@ -63,69 +54,63 @@ def build_latex(**options):
     # Clean up all the non-PDF files
     for name in os.listdir(kwargs['cwd']):
         full_path = '%s/%s' % (os.path.dirname(kwargs['cwd']), name)
-        
+
         if full_path[-3:] != 'pdf' and full_path[-4:] != '.svn':
             if os.path.isdir(full_path):
                 shutil.rmtree(full_path)
             else:
                 os.remove(full_path)
 
-def main(**options):
+def main(options):
     # Create temp dir and checkout SVN there
-    options['temp'] = tempfile.mkdtemp()
-    debug_print('Using "%(temp)s" temp dir', **options)
+    options.temp = tempfile.mkdtemp()
+    debug_print('Using "%s" temp dir' % options.temp, options)
 
-    Popen('svn co %s %s' % (CEC_MAYAVI_SVN, options['temp']),
-        stdout=options['stdout'], shell=True).wait()
+    Popen('svn co %s %s' % (CEC_MAYAVI_SVN, options.temp),
+        stdout=find_stdout(options), shell=True).wait()
 
     # Build docs
-    build_html(**options)
-    build_latex(**options)
+    build_html(options)
+    build_latex(options)
 
     # Checkin HTML to CEC SVN.
-    Popen('svn commit %(temp)s -m "%(message)s"' % options,
-        stdout=options['stdout'], shell=True).wait()
+    Popen('svn commit %s -m "%s"' % (options.temp, options.commit_message),
+        stdout=find_stdout(options), shell=True).wait()
 
     # Removed temp directory
-    shutil.rmtree(options['temp'])
+    shutil.rmtree(options.temp)
 
-class Usage(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+def handle_exec():
+    # Handle options
+    parser = OptionParser()
 
-def handle_exec(argv=None):
-    if argv is None:
-        argv = sys.argv
-    try:
-        try:
-            opts, args = getopt.getopt(argv[1:], 'hd:m:v',
-                ['help', 'docsrc=', 'message=', 'verbose'])
-        except getopt.error, msg:
-            raise Usage(msg)
+    parser.add_option('-d', '--docsrc', action='store',
+        default=os.path.join(os.path.abspath(os.path.dirname(__file__)),
+            'docs', 'mayavi', 'user_guide', 'source'),
+        help='read the documentation from the documentation source')
 
-        options = {
-            'docsrc': 'docs/mayavi/user_guide/source/',
-            'message': 'Update docs',
-        }
+    parser.add_option('-m', '--commit-message', action='store',
+        default='Updating docs',
+        help='use commit message when committing to code.enthought.com\'s ' \
+            'repo')
 
-        for option, value in opts:
-            if option in ('-v', '--verbose'):
-                options['stdout'] = None
-            if option in ('-h', '--help'):
-                raise Usage(help_message)
-            if option in ('-d', '--docsrc'):
-                options['docsrc'] = value
-            if option in ('-m', '--message'):
-                options['message'] = value
+    parser.add_option('-v', '--verbose', action='store_true',
+        help='print output of all commands which this executes [default]',)
 
-        if not options.has_key('stdout'):
-            options['stdout'] = open(os.devnull, 'w')
+    parser.add_option('-q', '--quiet', action='store_false', dest='verbose',
+        help='suppress the output of all commands which this executes')
 
-        return main(**options)
+    parser.set_defaults(**{
+        'docsrc': os.path.join(os.path.abspath(os.path.dirname(__file__)),
+            'docs', 'mayavi', 'user_guide', 'source'),
+        'commit_message': 'Updating docs',
+        'verbose': False,
+    })
 
-    except Usage, err:
-        print >> sys.stderr, sys.argv[0].split('/')[-1] + ': ' + str(err.msg)
-        return 0
+    options, args = parser.parse_args()
+
+    # Begin execution
+    main(options)
 
 if __name__ == '__main__':
-    sys.exit(handle_exec())
+    handle_exec()
