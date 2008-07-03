85e72b9e62c8d9546a1d8f5537ece30f6adb2bd7
gvaroquaux <gvaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-03 19:46:07 +0000

    ENH: mayavi wizard: remove the need for a handler.

--- enthought/mayavi/tools/wizards/wizard.py
+++ enthought/mayavi/tools/wizards/wizard.py
@@ -1,23 +1,26 @@
 
-from enthought.traits.api import HasTraits, Dict, Property, Str
-from enthought.traits.ui.api import EnumEditor, View, Item, Handler
-from enthought.traits.ui.menu import OKCancelButtons
-from enthought.pyface.api import GUI
-
-############################################################################
-# The WizardHandler class
-############################################################################
-class WizardHandler(Handler):
-    
-    def closed(self, info, is_ok):
-        if is_ok:
-            info.object._build_data_source()
+from enthought.traits.api import HasTraits, Dict, Property, Str, Button
+from enthought.traits.ui.api import EnumEditor, View, Item, HGroup, \
+    Spring
 
 
 ############################################################################
 # The Wizard class
 ############################################################################
 class Wizard(HasTraits):
+
+    ok_button = Button(label='OK')
+
+    cancel_button = Button(label='Cancel')
+
+    def _ok_button_fired(self):
+        if self.ui:
+            self.ui.dispose()
+            self._build_data_source()
+
+    def _cancel_button_fired(self):
+        if self.ui:
+            self.ui.dispose()
 
     data_sources = Dict
 
@@ -28,24 +31,29 @@ class Wizard(HasTraits):
     def _get__data_sources_labels(self):
         return self.data_sources.keys()
 
-    view = View(
-                Item('_selected_data_source', 
-                    editor=EnumEditor(name='_data_sources_labels')
-                    ), 
-                handler=WizardHandler(),
-                buttons=OKCancelButtons,
-                title='Import arrays wizard'
-                )
-
+    view = View(Item('_selected_data_source', 
+                    editor=EnumEditor(name='_data_sources_labels')), 
+                HGroup(Spring(), 
+                      'cancel_button', 
+                      'ok_button', 
+                      show_labels=False),
+                    title='Import arrays wizard')
 
     def _build_data_source(self):
         """ This is where we apply the selections made by the user in
             in the wizard to build the data source.
         """
         print "building source"
-   
+  
+
+    def wizard_view(self):
+        """ Pops up the view of the wizard, and keeps the reference it to
+            be able to close it.
+        """
+        self.ui = self.edit_traits()
+
 
 wizard = Wizard(data_sources={'1':1, '2':2, '3':3})
 
-wizard.edit_traits()
+wizard.wizard_view()
 
