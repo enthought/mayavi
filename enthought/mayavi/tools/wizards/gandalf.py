103b06f9cf76754d3d82dd3ef0ac09b54fab9ecd
fcady <fcady@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-03 22:13:20 +0000

    still in draft form

--- enthought/mayavi/tools/wizards/gandalf.py
+++ enthought/mayavi/tools/wizards/gandalf.py
@@ -21,6 +21,7 @@ class _Box(HasTraits):
     _item = Property
     
     def __init__(contents):
+        print "initing a box"
         self.contents = contents
     
     def _get__item(self):
@@ -37,18 +38,28 @@ class Wizard(HasTraits):
     
     data_list = List(_Box)
     
-    def _data_list_changed(self):
-        data_list = [_Box(key) for key in data_sources.getkeys()]
+    def _data_sources_changed(self):
+        print "The list should change now"
+        print self.data_sources.keys()
+        self.data_list = [ _Box(key) for key in self.data_sources.keys() ]
         
     view = Property
     
     def _get_view(self):
-        return View([box._item for box in data_list],
-                    buttons=OKCancelButtons,
-                    title='Import arrays wizard')
+        return View([box._item for box in self.data_list],
+                        buttons=OKCancelButtons,
+                        title='Import arrays wizard')
   
-        
+                    
 wizard = Wizard(data_sources={'1':11, '2':21, '5':31})
+
+print "here I am"
+
+print wizard.data_sources
+
+print "more"
+
+print wizard.data_list
 
 wizard.edit_traits()
 
