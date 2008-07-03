6c5964e2de1ef471e8032a64645b4531da82c406
gvaroquaux <gvaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-03 15:23:51 +0000

    ENH: mayavi wizard: wire up the build_data_source callback.

--- enthought/mayavi/tools/wizards/wizard.py
+++ enthought/mayavi/tools/wizards/wizard.py
@@ -1,7 +1,22 @@
 
 from enthought.traits.api import HasTraits, Dict, Property, Str
-from enthought.traits.ui.api import EnumEditor, View, Item
+from enthought.traits.ui.api import EnumEditor, View, Item, Handler
+from enthought.traits.ui.menu import OKCancelButtons
+from enthought.pyface.api import GUI
 
+############################################################################
+# The WizardHandler class
+############################################################################
+class WizardHandler(Handler):
+    
+    def closed(self, info, is_ok):
+        if is_ok:
+            info.object._build_data_source()
+
+
+############################################################################
+# The Wizard class
+############################################################################
 class Wizard(HasTraits):
 
     data_sources = Dict
@@ -14,7 +29,18 @@ class Wizard(HasTraits):
         return self.data_sources.keys()
 
     view = View(Item('_selected_data_source', 
-                    editor=EnumEditor(name='_data_sources_labels')))
+                    editor=EnumEditor(name='_data_sources_labels')), 
+                    handler=WizardHandler(),
+                    buttons=OKCancelButtons,
+                    title='Import arrays wizard')
+
+
+    def _build_data_source(self):
+        """ This is where we apply the selections made by the user in
+            in the wizard to build the data source.
+        """
+        print "building source"
+   
 
 wizard = Wizard(data_sources={'1':1, '2':2, '3':3})
 
