7038dae5f95aa9a30d3b6582d2bc11370e274356
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-05 03:58:41 +0000

    Cleanup

--- examples/tvtk/visual/orbit.py
+++ examples/tvtk/visual/orbit.py
@@ -4,25 +4,36 @@ the in a scene using visual modeule."""
 # Author: Raashid Baig <raashid@aero.iitb.ac.in>
 # License: BSD Style.
 
-from enthought.tvtk.tools.visual import *
 from math import sqrt
 
+from enthought.tvtk.tools.visual import sphere, iterate, show, vector, curve
+
 #Creating the actors for the scene
-giant = sphere( pos = (-1.0e11, 0, 0), radius = 2e10, color = (1, 0, 0), mass = 2e30)
-dwarf = sphere( pos = (1.5e11, 0, 0), radius = 1e10, color = (1, 1, 0), mass = 1e30)
-giant.p = vector(0, 0, -1e4)*giant.mass
-dwarf.p = -1*giant.p    
+giant = sphere(pos=(-1.0e11, 0, 0),
+               radius=2e10,
+               color=(1, 0, 0),
+               mass=2e30)
+
+dwarf = sphere(pos=(1.5e11, 0, 0),
+               radius=1e10,
+               color=(1, 1, 0),
+               mass=1e30)
+
+giant.p = vector(0, 0, -1e4) * giant.mass
+dwarf.p = -1*giant.p
+
 # creating the curve which will trace the paths of actors
 for a in [giant, dwarf]:
-    a.orbit = curve(radius = 2e9, color = a.color)
-    
+    a.orbit = curve(radius=2e9, color=a.color)
+
 dt = 86400
 
 def anim():
     #Creating the animation function which will be called at
     #uniform timeperiod through the iterate function
     dist = dwarf.pos - giant.pos
-    force = 6.7e-11 * giant.mass * dwarf.mass * dist/(sqrt(dist[0]**2 + dist[1]**2 + dist[2]**2))**3
+    force = 6.7e-11 * giant.mass * dwarf.mass * \
+        dist/(sqrt(dist[0]**2 + dist[1]**2 + dist[2]**2))**3
     giant.p = giant.p + force*dt
     dwarf.p = dwarf.p - force*dt
 
@@ -36,4 +47,3 @@ def anim():
 a = iterate(50, anim)
 a.edit_traits()
 show()
-
