626bae8454326bbaa76d6345e8116e022693d179
jwheeler <jwheeler@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-07 19:14:19 +0000

    Add infrastructure for building docs locally

--- commit_docs.py
+++ commit_docs.py
@@ -5,9 +5,7 @@ import os.path
 import shutil
 from subprocess import Popen
 import tempfile
-
-# Only publish the build functions to other Python modules looking at this.
-__all__ = ['build_html', 'build_latex']
+import zipfile
 
 CEC_MAYAVI_SVN = 'https://svn.enthought.com/svn/cec/trunk/projects/mayavi'
 
@@ -50,7 +48,22 @@ def build_latex(docsrc, target, stdout, verbose=False):
             else:
                 os.remove(full_path)
 
-def main(options):
+def build_zip(docsrc, target, stdout, verbose=False):
+    """ Read from the html directory in options.target and ZIP that folder. """
+    if verbose: 'Creating ZIP archive of target\'s html'
+
+    zf = zipfile.ZipFile(os.path.join(target, 'html_docs.zip'), 'w')
+
+    # Write files in html/ to zf
+    length = len(os.path.abspath(os.path.join(target, 'html')))
+    subtract_base = lambda root: root[length+1:]
+    for root, dirs, files in os.walk(os.path.join(target, 'html')):
+        baseless = subtract_base(root)
+        if not baseless.startswith('.doctrees'):
+            for f in files:
+                zf.write(os.path.join(root, f), os.path.join(baseless, f))
+
+def svn_commit(options):
     if options.verbose: 'Using "%s" target dir' % options.target
 
     # Create stdout attribute of options
@@ -65,7 +78,7 @@ def main(options):
 
     # Build docs
     kwargs = dict((k, getattr(options, k)) for k in
-        ('docsrc', 'target', 'stdout', 'verbose'))
+                  ('docsrc', 'target', 'stdout', 'verbose'))
 
     build_html(**kwargs)
     build_latex(**kwargs)
@@ -81,7 +94,25 @@ def main(options):
     # Removed target directory
     shutil.rmtree(options.target)
 
-def handle_exec():
+def rebuild_docs(options):
+    """ Create a ZIP with the documentation to the Mayavi SVN. """
+
+    # This should be moved to main(); currently it's a duplicate of the same
+    # code in svn_commit().
+    if options.verbose:
+        options.stdout = None
+    else:
+        options.stdout = open(os.devnull, 'w')
+
+    # zipfile = zipfile.ZipFile('docs/mayavi/user_guide/html_docs.zip')
+    kwargs = dict((k, getattr(options, k)) for k in
+                  ('docsrc', 'target', 'stdout', 'verbose'))
+
+    build_html(**kwargs)
+    build_latex(**kwargs)
+    build_zip(**kwargs)
+
+def main():
     # Handle options
     parser = OptionParser()
 
@@ -108,14 +139,27 @@ def handle_exec():
         'docsrc': os.path.join(os.path.abspath(os.path.dirname(__file__)),
             'docs', 'mayavi', 'user_guide', 'source'),
         'commit_message': 'Updating docs',
-        'target': tempfile.mkdtemp(),
+        'target': None,
         'verbose': False,
     })
 
     options, args = parser.parse_args()
 
-    # Begin execution
-    main(options)
+    if len(args) == 1:
+        command = args[0]
+    else:
+        parser.error('only one command should be given')
+
+    if command == 'build':
+        if options.target is None:
+            options.target = os.path.abspath('docs/mayavi/user_guide/')
+        rebuild_docs(options)
+    elif command == 'update-web':
+        if options.target is None:
+            options.target = tempfile.mkdtemp()
+        svn_commit(options)
+    else:
+        parser.error('only valid commands are "build" and "update-web"')
 
 if __name__ == '__main__':
-    handle_exec()
+    main()
