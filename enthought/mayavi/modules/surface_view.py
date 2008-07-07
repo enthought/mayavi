e636e18853d1ef5b104efabb5f827475e1e652f2
judah <judah@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-07 21:52:57 +0000

    Minor source reformatting.

--- enthought/mayavi/modules/surface_view.py
+++ enthought/mayavi/modules/surface_view.py
@@ -1,9 +1,12 @@
 from enthought.traits.ui.api import Item, Group, View, InstanceEditor
 from enthought.mayavi.components.actor_view import actor_view, texture_view
 
-view = View(Group(Item(name='enable_contours', label='Enable Contours'),
-                  Group(Item(name='contour', style='custom',
-                             enabled_when='object.enable_contours'),
+view = View(
+            Group(Item(name='enable_contours', label='Enable Contours'),
+                  Group(Item(name='contour', 
+                             style='custom',
+                             enabled_when='object.enable_contours'
+                             ),
                         show_labels=False,
                   ),
                   show_labels=True,
@@ -11,15 +14,19 @@ view = View(Group(Item(name='enable_contours', label='Enable Contours'),
             ),
             
             Group(Item('actor', 
-                       resizable=True, style='custom',
-                       editor=InstanceEditor(view=actor_view)),
+                       resizable=True, 
+                       style='custom',
+                       editor=InstanceEditor(view=actor_view)
+                       ),
                   label='Actor',
                   show_labels=False,
             ),
                   
             Group(Item('actor', 
-                       resizable=True, style='custom',
-                       editor=InstanceEditor(view=texture_view)),
+                       resizable=True, 
+                       style='custom',
+                       editor=InstanceEditor(view=texture_view)
+                       ),
                   label='Texturing',
                   show_labels=False,
             ),       
