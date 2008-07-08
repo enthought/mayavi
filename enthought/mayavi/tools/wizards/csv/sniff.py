a951223fd06d79f170aa9213b89788b9ae12637c
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-08 02:13:00 +0000

    Improved various details of the CSV sniffer.

--- enthought/mayavi/tools/wizards/csv/sniff.py
+++ enthought/mayavi/tools/wizards/csv/sniff.py
@@ -1,49 +1,90 @@
 #!/usr/bin/env python
-import sys
 import csv
 
-import numpy
 
-def sniff(filename):
+class Sniff:
+    """ Sniff a CSV file and determine some of it's properties.
+
+        The properties determined here allow an CSV of unknown format
+        to be read by numpy.loadtxt, i.e. the methods and attributes
+        are suitable to determine required keyword arguments for
+        numpy.loadtxt
+    """
+    def __init__(self, filename):
+        self.lines = []
+        for line in open(filename, 'rb'):
+            line = line.strip()
+            self.lines.append(line)
+            if line:
+                dataline = line
+            if len(self.lines) > 10:
+                break
+            
+        self.dialect = csv.Sniffer().sniff(dataline)
+        
+        self.datatypes = self._datatypes(dataline)
+        
+        self.numcols = len(self.datatypes)
+        
+    def _show(self):
+        print self.lines
+        print self.datatypes
+        for a in dir(self.dialect):
+            if a.startswith('_'):
+                continue
+            print '%20s: %r' % (a, getattr(self.dialect, a))
+
+    def _split(self, line):
+        delimiter = self.dialect.delimiter
+        if delimiter == ' ':
+            delimiter = None
+        return line.split(delimiter)
     
-    firstlines = []
+    def delimiter(self):
+        return self.dialect.delimiter
     
-    for line in open(filename, 'rb'):
-        firstlines.append(line.strip())
-        if len(firstlines) > 10:
-            break
+    def skiprows(self):
+        for n, line in enumerate(self.lines):
+            if self.datatypes == self._datatypes(line):
+                return n
     
-    last = -1
-    while not firstlines[last]:
-        last -= 1
+    def col_names(self):
+        for line in self.lines:
+            if len(self._split(line)) == self.numcols and \
+                   self.datatypes != self._datatypes(line):
+                return [title.strip('"\' ') for title in self._split(line)]
+        return ['Column %i' % (i+1) for i in xrange(self.numcols)]
 
-    lastline = firstlines[last]
-    print ';;;;;;;;;;'
-    print repr(lastline)
-    dialect = csv.Sniffer().sniff(lastline)
+    def _datatypes(self, line):
+        res = []
+        for s in self._split(line):
+            try:
+                float(s)
+                res.append(float)
+            except ValueError:
+                res.append(str)
+        return res
+
+
+if __name__ == '__main__':
+    import sys
+    import numpy
     
-    for a in dir(dialect):
-        if a.startswith('_'):
-            continue
-        print '%20s: %r' % (a, getattr(dialect, a))
-
-    res = {
-        'delimiter': dialect.delimiter,
-        'skiprows': 0,
-        }
+    fname = sys.argv[1]
     
-    return res
-
-
-
-def foo(**kwds):
-    for kv in kwds.iteritems():
-        print '%30s: %r' % kv
-
-
-fname = sys.argv[1]
-
-
-foo(**sniff(fname))
-
-#numpy.loadtxt(fname, **sniff(fname))
+    x = Sniff(fname)
+    x._show()
+    
+    print 'delimiter: %r' % x.delimiter()
+    print 'skiprows : %r' % x.skiprows()
+    print 'numcols  : %r' % x.numcols
+    print 'datatypes: %r' % x.datatypes
+    print 'col_names: %r' % x.col_names()
+    
+    delimiter = x.delimiter()
+    if delimiter == ' ':
+        delimiter = None
+    
+    print numpy.loadtxt(fname,
+                        delimiter=delimiter,
+                        skiprows=x.skiprows())
