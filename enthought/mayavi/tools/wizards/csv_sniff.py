6d63ebfe0d0b3cdd7dce46d062bac4035f6c2f0f
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-08 21:12:13 +0000

    Added useful helper functions.

--- enthought/mayavi/tools/wizards/csv_sniff.py
+++ enthought/mayavi/tools/wizards/csv_sniff.py
@@ -1,5 +1,11 @@
-#!/usr/bin/env python
+# Author: Ilan Schnell <ischnell@enthought.com>
+# Copyright (c) 2008, Ilan Schnell, Enthought, Inc.
+# License: BSD Style.
+
 import csv
+
+# FIXME, see loadtxt.py (should really be the loadtxt from numpy)
+from enthought.mayavi.tools.wizards.loadtxt import loadtxt
 
 
 class Sniff:
@@ -9,11 +15,18 @@ class Sniff:
         to be read by numpy.loadtxt, i.e. the methods and attributes
         are suitable to determine required keyword arguments for
         numpy.loadtxt
+
+        For a usecase, see the loadtxt_unknown function below.
     """
     def __init__(self, filename):
         self.lines = self._read_few_lines(filename)
         self.reallines = [line for line in self.lines if line.strip()]
         self.dialect = csv.Sniffer().sniff(self.reallines[0])
+
+        self._usePySplit = False
+        if self.dialect.delimiter == ' ':
+            self._usePySplit = True
+        
         self.numcols = len(self._split(self.reallines[0]))
         
         for rl in self.reallines:
@@ -30,17 +43,18 @@ class Sniff:
             if len(res) > 10:
                 break
         return res
-
+    
     def _show(self):
-        print self._split(self.reallines[-1])
-        print self.numcols
         for a in dir(self.dialect):
             if a.startswith('_'):
                 continue
             print '%20s: %r' % (a, getattr(self.dialect, a))
     
     def _split(self, line):
-        return csv.reader([line], self.dialect).next()
+        if self._usePySplit:
+            return line.split()
+        else:
+            return csv.reader([line], self.dialect).next()
     
     def delimiter(self):
         return self.dialect.delimiter
@@ -91,29 +105,42 @@ class Sniff:
         return tuple(res)
 
 
-if __name__ == '__main__':
-    import sys
-    import numpy
+def loadtxt_unknown(filename, verbose=False):
+    """ Like numpy.loadtxt but more general, in the sense that it uses
+        Sniff first to determine the necessary keyword arguments for loadtxt.
+    """
+    s = Sniff(filename)
+
+    if verbose:
+        s._show()
     
-    fname = sys.argv[1]
-    
-    x = Sniff(fname)
-    x._show()
-    
-    print 'delimiter: %r' % x.delimiter()
-    print 'skiprows : %r' % x.skiprows()
-    print 'numcols  : %r' % x.numcols
-    print 'datatypes: ' + repr(x.datatypes)
-    print 'names    : ' + repr(x.names())
-    print 'formats  : ' + repr(x.formats())
-    print 'dtype    : %r' % x.dtype()
-    
-    delimiter = x.delimiter()
+    delimiter = s.delimiter()
     if delimiter == ' ':
         delimiter = None
+
+    return loadtxt(filename,
+                   delimiter=delimiter,
+                   dtype=s.dtype(),
+                   skiprows=s.skiprows())
+
+
+def array2dict(arr):
+    """ Takes an array with  special names dtypes and returns a dict where
+        each name is a key and the corresponding data (as a 1d array) is the
+        value.
+    """
+    d = {}
+    for k in arr.dtype.names:
+        d[k] = arr[k]
+        
+    return d
+
+
+if __name__ == '__main__':
+    import sys
     
-    print repr(numpy.loadtxt(fname,
-                             delimiter=delimiter,
-                             dtype=x.dtype(),
-                             skiprows=x.skiprows()))
+    fname = sys.argv[1]
+
+    x = loadtxt_unknown(fname, verbose=1)
     
+    print array2dict(x)
