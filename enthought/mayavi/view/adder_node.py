f70e1a5795dc1197455302558b933e4f8a295444
judah <judah@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-09 15:02:17 +0000

    The start of a new class for nodes that add other nodes to the mayavi tree.

---
+++ enthought/mayavi/view/adder_node.py
@@ -0,0 +1,12 @@
+"""
+A custom node for a Tree Editor that provides views for adding various nodes
+to the tree.
+"""
+# Author: Judah De Paula <judah@enthought.com>
+# Copyright (c) 2008, Enthought, Inc.
+# License: BSD Style.
+from enthought.mayavi.core.base import Base
+from enthought.traits.api import List
+
+class AdderNode(Base):
+    children = List()
