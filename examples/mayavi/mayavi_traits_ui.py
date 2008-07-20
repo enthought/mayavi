d0ea4d42ef046e86577f25854e1a23218b3e236d
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-20 10:04:48 +0000

    Updating to use new location for engine_view.

--- examples/mayavi/mayavi_traits_ui.py
+++ examples/mayavi/mayavi_traits_ui.py
@@ -17,7 +17,7 @@ from numpy import sqrt, sin, mgrid
 from enthought.traits.api import HasTraits, Instance, Property, Enum
 from enthought.traits.ui.api import View, Item, HSplit, VSplit, InstanceEditor
 from enthought.tvtk.pyface.scene_editor import SceneEditor 
-from enthought.mayavi.view.engine_view import EngineView
+from enthought.mayavi.core.ui.engine_view import EngineView
 from enthought.mayavi import mlab
 from enthought.mayavi.tools.mlab_scene_model import MlabSceneModel
 
