38841c9d15501e47083f7694cda2b3b86280297b
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-26 09:20:03 +0000

    ENH: Enhancing the write_data function to automatically supply file extensions if none are provided.  Also supports writing out classic .vtk files.  This is all tested.

--- enthought/tvtk/misc.py
+++ enthought/tvtk/misc.py
@@ -4,6 +4,8 @@
 # Copyright (c) 2007,  Enthought, Inc.
 # License: BSD Style.
 
+from os.path import splitext
+
 # We import from tvtk.py and not api.py to prevent circular imports.
 from enthought.tvtk.tvtk import tvtk
 
@@ -13,11 +15,42 @@ from enthought.tvtk.tvtk import tvtk
 def write_data(dataset, fname):
     """Given a TVTK `dataset` this writes the `dataset` to a VTK XML
     file having file name `fname`.
+
+    If the given file name has no extension, one is automatically picked
+    based on the dataset and an XML file is written out.  If the
+    filename has a '.vtk' extension an old style VTK file is written.
+    If any other extension is specified, an XML file is written out with
+    the given extension.
     """
+
     err_msg = "Can only write tvtk.DataSet instances "\
               "'got %s instead"%(dataset.__class__.__name__)
     assert isinstance(dataset, tvtk.DataSet), err_msg
 
-    w = tvtk.XMLDataSetWriter(file_name=fname, input=dataset)
+    # Mapping to determine appropriate extension and writer.
+    d2r = {'vtkImageData': ('.vti', tvtk.StructuredPointsWriter),
+           'vtkRectilinearGrid': ('.vtr', tvtk.RectilinearGridWriter),
+           'vtkStructuredGrid': ('.vts', tvtk.StructuredGridWriter),
+           'vtkPolyData': ('.vtp', tvtk.PolyDataWriter),
+           'vtkUnstructuredGrid': ('.vtu', tvtk.UnstructuredGridWriter)
+           }
+    
+    for type in d2r:
+        if dataset.is_a(type):
+            datatype = d2r[type]
+            break
+
+    ext = splitext(fname)[1]
+    if ext == '.vtk':
+        file_name = fname 
+        writer = datatype[1]
+    elif len(ext) == 0:
+        file_name = fname + datatype[0] 
+        writer = tvtk.XMLDataSetWriter
+    else:
+        file_name = fname
+        writer = tvtk.XMLDataSetWriter
+
+    w = writer(file_name=file_name, input=dataset)
     w.write()
     
---
+++ enthought/tvtk/tests/test_misc.py
@@ -0,0 +1,77 @@
+"""
+Tests for enthought/tvtk/misc.py
+"""
+# Author: Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
+# Copyright (c) 2008, Enthought, Inc.
+# License: BSD Style.
+
+import unittest
+import tempfile
+import os.path
+import os
+
+from enthought.tvtk.api import tvtk, write_data
+
+class TestMisc(unittest.TestCase):
+    def setUp(self):
+
+        datasets = [tvtk.ImageData(),
+                    tvtk.StructuredPoints(),
+                    tvtk.RectilinearGrid(),
+                    tvtk.StructuredGrid(),
+                    tvtk.PolyData(),
+                    tvtk.UnstructuredGrid(),
+                    ]
+        exts = ['.vti', '.vti', '.vtr', '.vts', '.vtp', '.vtu']
+        self.datasets = datasets
+        self.exts = exts
+
+    def test_write_data_xml_noext(self):
+        "XML file writing without extensions"
+        # Check if write_data writes out XML files with the correct
+        # extension when none is specified.
+
+        datasets = self.datasets
+        exts = self.exts
+
+        for d, ext in zip(datasets, exts):
+            fh, fname = tempfile.mkstemp(ext)
+            fbase = os.path.splitext(fname)[0]
+            os.close(fh)
+            os.remove(fname)
+            write_data(d, fbase)
+            self.assertEqual(os.path.exists(fname), True)
+            os.remove(fname)
+
+    def test_write_data_xml(self):
+        "XML file writing with specified extension"
+        datasets = self.datasets
+
+        for d in datasets:
+            fh, fname = tempfile.mkstemp('.xml')
+            os.close(fh)
+            os.remove(fname)
+            self.assertEqual(os.path.exists(fname), False)
+            write_data(d, fname)
+            self.assertEqual(os.path.exists(fname), True)
+            os.remove(fname)
+
+    def test_write_data_vtk(self):
+        "Old-style VTK file writing with specified extension"
+        datasets = self.datasets
+
+        for d in datasets:
+            fh, fname = tempfile.mkstemp('.vtk')
+            os.close(fh)
+            os.remove(fname)
+            self.assertEqual(os.path.exists(fname), False)
+            write_data(d, fname)
+            self.assertEqual(os.path.exists(fname), True)
+            r = tvtk.DataSetReader(file_name=fname)
+            r.update()
+            self.assertEqual(isinstance(r.output, d.__class__), True)
+            os.remove(fname)
+
+
+if __name__ == '__main__':
+    unittest.main()
