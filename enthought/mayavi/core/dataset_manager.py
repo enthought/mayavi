23db2e97c1a8e8cca58011715aa4053357e1f68f
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-15 21:00:04 +0000

    NEW: Adding a fully tested data set manager that lets users add/remove/modify attribute arrays to a tvtk dataset.  This is fully tested and also does not influence any other code.  I just wanted this checked in before the release so the code is available for developers to use.

---
+++ enthought/mayavi/core/dataset_manager.py
@@ -0,0 +1,244 @@
+"""
+Code to help with managing a TVTK data set in Pythonic ways.
+"""
+
+# Author: Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
+# Copyright (c) 2008, Enthought, Inc.
+# License: BSD Style.
+
+from enthought.traits.api import (HasTraits, Instance, Array, Str, 
+                        Property, Dict)
+from enthought.tvtk.api import tvtk
+from enthought.tvtk.array_handler import array2vtk
+
+
+######################################################################
+# Utility functions.
+######################################################################
+def get_array_type(arr):    
+    """Returns if the array is a scalar ('scalars'), vector
+    ('vectors') or tensor ('tensors').  It looks at the number of
+    components to decide.  If it has a wierd number of components it
+    returns the empty string.
+    """    
+    n = arr.number_of_components
+    ret = {1: 'scalars', 3: 'vectors', 4: 'scalars', 9:'tensors'}
+    return ret.get(n) or ''
+
+
+def get_attribute_list(data):
+    """ Gets scalar, vector and tensor information from the given data
+    (either cell or point data).
+    """    
+    attr = {'scalars':[], 'vectors':[], 'tensors':[]}
+    if data is not None:
+        n = data.number_of_arrays
+        for i in range(n):
+            name = data.get_array_name(i)
+            t = get_array_type(data.get_array(i))
+            if len(t) > 0 and name is not None:
+                attr[t].extend([name])
+    
+    def _mk_first(lst, value):
+        """Makes the specified `value` the first item in `lst`."""
+        lst.remove(value)
+        lst.insert(0, value)
+    
+    attr1 = attr.copy()
+    for a in attr:
+        v = getattr(data, a)
+        if v is not None:
+            name = v.name
+            if name is not None:
+                try:
+                    _mk_first(attr[a], v.name)
+                except ValueError:
+                    # Sometimes we have a multi-component scalar.
+                    attr1[a].insert(0, name)
+    return attr1
+
+
+def get_all_attributes(obj):
+    """Gets the scalar, vector and tensor attributes that are
+    available in the given VTK data object.
+    """
+    point_attr = get_attribute_list(obj.point_data)
+    cell_attr = get_attribute_list(obj.cell_data)
+    return point_attr, cell_attr    
+    
+
+################################################################################
+# `DatasetManager` class.
+################################################################################ 
+class DatasetManager(HasTraits):
+
+    # The TVTK dataset we manage.
+    dataset = Instance(tvtk.DataSet)
+
+    # Our output, this is the dataset modified by us with different
+    # active arrays.
+    output = Property(Instance(tvtk.DataSet))
+
+    # The point scalars for the dataset.  You may manipulate the arrays
+    # in-place.  However adding new keys in this dict will not set the
+    # data in the `dataset` for that you must explicitly call
+    # `add_array`.
+    point_scalars = Dict(Str, Array)
+    # Point vectors.
+    point_vectors = Dict(Str, Array)
+    # Point tensors.
+    point_tensors = Dict(Str, Array)
+
+    # The cell scalars for the dataset.
+    cell_scalars = Dict(Str, Array)
+    cell_vectors = Dict(Str, Array)
+    cell_tensors = Dict(Str, Array)
+
+    # This filter allows us to change the attributes of the data
+    # object and will ensure that the pipeline is properly taken care
+    # of.  Directly setting the array in the VTK object will not do
+    # this.
+    _assign_attribute = Instance(tvtk.AssignAttribute, args=(),
+                                 allow_none=False)
+
+
+    ######################################################################
+    # Public interface.
+    ######################################################################
+    def add_array(self, array, name, category='point'):
+        """
+        Add an array to the dataset to specified category ('point' or
+        'cell').
+        """
+        assert len(array.shape) <= 2, "Only 2D arrays can be added."
+        data = getattr(self.dataset, '%s_data'%category)
+        if len(array.shape) == 2:
+            assert array.shape[1] in [1, 3, 4, 9], \
+                    "Only Nxm arrays where (m in [1,3,4,9]) are supported"
+            va = tvtk.to_tvtk(array2vtk(array))
+            va.name = name
+            data.add_array(va)
+            mapping = {1:'scalars', 3: 'vectors', 4: 'scalars', 
+                       9: 'tensors'}
+            dict = getattr(self, '%s_%s'%(category,
+                                          mapping[array.shape[1]]))
+            dict[name] = array
+        else:
+            va = tvtk.to_tvtk(array2vtk(array))
+            va.name = name
+            data.add_array(va)
+            dict = getattr(self, '%s_scalars'%(category))
+            dict[name] = array
+
+    def remove_array(self, name, category='point'):
+        """Remove an array by its name and optional category (point and
+        cell).  Returns the removed array.
+        """
+        type = self._find_array(name, category)
+        data = getattr(self.dataset, '%s_data'%category)
+        data.remove_array(name)
+        d = getattr(self, '%s_%s'%(category, type))
+        return d.pop(name)
+
+    def rename_array(self, name1, name2, category='point'):
+        """Rename a particular array from `name1` to `name2`.
+        """
+        type = self._find_array(name1, category)
+        data = getattr(self.dataset, '%s_data'%category)
+        arr = data.get_array(name1)
+        arr.name = name2
+        d = getattr(self, '%s_%s'%(category, type))
+        d[name2] = d.pop(name1)
+
+    def activate(self, name, category='point'):
+        """Make the specified array the active one.
+        """
+        type = self._find_array(name, category)
+        self._activate_data_array(type, category, name)
+
+    def update(self):
+        """Update the dataset when the arrays are changed.
+        """
+        self.dataset.modified()
+        self._assign_attribute.update()
+
+    ######################################################################
+    # Non-public interface.
+    ######################################################################
+    def _dataset_changed(self, value):
+        self._setup_data()
+        self._assign_attribute.input = value
+
+    def _get_output(self):
+        return self._assign_attribute.output
+
+    def _setup_data(self):
+        """Updates the arrays from what is available in the input data.
+        """
+        input = self.dataset
+        pnt_attr, cell_attr = get_all_attributes(input)
+
+        self._setup_data_arrays(cell_attr, 'cell')
+        self._setup_data_arrays(pnt_attr, 'point')
+     
+    def _setup_data_arrays(self, attributes, d_type):
+        """Given the dict of the attributes from the
+        `get_all_attributes` function and the data type (point/cell)
+        data this will setup the object and the data.  
+        """
+        attrs = ['scalars', 'vectors', 'tensors']
+        aa = self._assign_attribute
+        input = self.dataset
+        data = getattr(input, '%s_data'%d_type)
+        for attr in attrs:
+            values = attributes[attr]
+            # Get the arrays from VTK, create numpy arrays and setup our
+            # traits.
+            arrays = {}
+            for name in values:
+                va = data.get_array(name)
+                npa = va.to_array()
+                va.from_array(npa)
+                arrays[name] = npa
+
+            setattr(self, '%s_%s'%(d_type, attr), arrays)
+
+    def _activate_data_array(self, data_type, category, name):
+        """Activate (or deactivate) a particular array.
+
+        Given the nature of the data (scalars, vectors etc.) and the
+        type of data (cell or points) it activates the array given by
+        its name.
+
+        Parameters:
+        -----------
+
+        data_type: one of 'scalars', 'vectors', 'tensors'
+        category: one of 'cell', 'point'.
+        name: string of array name to activate.
+        """
+        input = self.dataset
+        data = None
+        data = getattr(input, category + '_data')
+        method = getattr(data, 'set_active_%s'%data_type)
+        if len(name) == 0:
+            # If the value is empty then we deactivate that attribute.
+            method(None)
+        else:
+            aa = self._assign_attribute
+            method(name)
+            aa.assign(name, data_type.upper(), category.upper() +'_DATA')
+            aa.update()
+
+    def _find_array(self, name, category='point'):
+        """Return information on which kind of attribute contains the
+        specified named array in a particular category."""
+        types = ['scalars', 'vectors', 'tensors']
+        for type in types:
+            attr = '%s_%s'%(category, type)
+            d = getattr(self, attr)
+            if name in d.keys():
+                return type
+        raise KeyError('No %s array named %s available in dataset'
+                        %(category, name))
+
---
+++ enthought/mayavi/tests/test_dataset_manager.py
@@ -0,0 +1,228 @@
+"""
+Test for the dataset_manager.py module.
+"""
+# Author: Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
+# Copyright (c) 2008, Enthought, Inc.
+# License: BSD Style.
+
+import unittest
+import numpy as N
+
+from enthought.tvtk.api import tvtk
+from enthought.mayavi.core.dataset_manager import DatasetManager
+
+
+def make_data():
+    points = N.array([[0,0,0], [1,0,0], [0,1,0], [0,0,1], # tets
+                    [1,0,0], [2,0,0], [1,1,0], [1,0,1],
+                    [2,0,0], [3,0,0], [2,1,0], [2,0,1],
+                    ], 'f')
+    tets = N.array([[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10, 11]])
+    tet_type = tvtk.Tetra().cell_type
+    ug = tvtk.UnstructuredGrid(points=points)
+    ug.set_cells(tet_type, tets)
+    # Setup the point attributes.
+    temp = N.random.random(12)
+    v = N.random.randn(12, 3)
+    ten = N.random.randn(12, 9)
+    a = tvtk.FloatArray(name='p')
+    a.from_array(N.random.randn(12))
+    ug.point_data.add_array(a)
+    ug.point_data.scalars = temp
+    ug.point_data.scalars.name = 't'
+    ug.point_data.vectors = v
+    ug.point_data.vectors.name = 'v'
+    ug.point_data.tensors = ten
+    ug.point_data.tensors.name = 'ten'
+    # Setup the cell attributes.
+    temp = N.random.random(3)
+    v = N.random.randn(3, 3)
+    ten = N.random.randn(3, 9)
+    ug.cell_data.scalars = temp
+    ug.cell_data.scalars.name = 't'
+    ug.cell_data.vectors = v
+    ug.cell_data.vectors.name = 'v'
+    ug.cell_data.tensors = ten
+    ug.cell_data.tensors.name = 'ten'
+    return ug
+
+
+class TestDataSetManager(unittest.TestCase):
+    def setUp(self):
+        self.data = make_data()
+        self.dm = DatasetManager(dataset=self.data)
+        return
+
+    def tearDown(self):
+        return
+
+    def test_point_arrays(self):
+        "Are the point data arrays of the manager setup right?"
+        dm = self.dm
+        sc = dm.point_scalars.keys()
+        sc.sort()
+        self.assertEqual(sc, ['p', 't'])
+        vec = dm.point_vectors.keys()
+        self.assertEqual(vec, ['v'])
+        ten = dm.point_tensors.keys()
+        self.assertEqual(ten, ['ten'])
+       
+    def test_point_array_view(self):
+        "Are the manager's point arrays views of the VTK data?"
+        # Check that the array we have is really a view of the VTK data.
+        dm = self.dm
+        data = self.data
+        t = dm.point_scalars['t']
+        t[0] += 1.0
+        self.assertEqual(t[0], data.point_data.scalars[0])
+
+        v = dm.point_vectors['v']
+        v[0][0] += 1.0
+        self.assertEqual(v[0][0], data.point_data.vectors[0][0])
+
+        ten = dm.point_tensors['ten']
+        ten[0][0] += 1.0
+        self.assertEqual(ten[0][0], data.point_data.tensors[0][0])
+
+    def test_cell_arrays(self):
+        "Are the cell arrays of the manager setup right?"
+        dm = self.dm
+        sc = dm.cell_scalars.keys()
+        self.assertEqual(sc, ['t'])
+        vec = dm.cell_vectors.keys()
+        self.assertEqual(vec, ['v'])
+        ten = dm.cell_tensors.keys()
+        self.assertEqual(ten, ['ten'])
+       
+    def test_cell_array_view(self):
+        "Are the manager's cell arrays views of the VTK data?"
+        # Check that the array we have is really a view of the VTK data.
+        dm = self.dm
+        data = self.data
+        t = dm.cell_scalars['t']
+        t[0] += 1.0
+        self.assertEqual(t[0], data.cell_data.scalars[0])
+
+        v = dm.cell_vectors['v']
+        v[0][0] += 1.0
+        self.assertEqual(v[0][0], data.cell_data.vectors[0][0])
+
+        ten = dm.cell_tensors['ten']
+        ten[0][0] += 1.0
+        self.assertEqual(ten[0][0], data.cell_data.tensors[0][0])
+
+    def test_remove_array(self):
+        "Does the remove_array method work correctly."
+        dm = self.dm
+        data = self.data
+        dm.remove_array('t', 'point')
+        self.assertEqual(len(dm.point_scalars), 1)
+        self.assertEqual(dm.point_scalars.keys(), ['p'])
+        dm.remove_array('ten', 'point')
+        self.assertEqual(len(dm.point_tensors), 0)
+        # Make sure the default category is point.
+        dm.remove_array('v')
+        self.assertEqual(len(dm.point_vectors), 0)
+
+        # Cell arrays.
+        dm.remove_array('t', 'cell')
+        self.assertEqual(len(dm.cell_scalars), 0)
+        dm.remove_array('ten', 'cell')
+        self.assertEqual(len(dm.cell_tensors), 0)
+        dm.remove_array('v', 'cell')
+        self.assertEqual(len(dm.cell_vectors), 0)
+
+    def test_rename_array(self):
+        "Does array renaming work."
+        dm = self.dm
+        data = self.data
+
+        dm.rename_array('ten', 'ten1', 'point')
+        self.assertEqual(dm.point_tensors.keys(), ['ten1'])
+        pd = data.point_data
+        arrs = [pd.get_array_name(x) for x in
+                range(pd.number_of_arrays)]
+        arrs.sort()
+        self.assertEqual(arrs, ['p', 't', 'ten1', 'v'])
+
+        dm.rename_array('t', 'temp', 'cell')
+        self.assertEqual(dm.cell_scalars.keys(), ['temp'])
+        cd = data.cell_data
+        arrs = [cd.get_array_name(x) for x in
+                range(cd.number_of_arrays)]
+        arrs.sort()
+        self.assertEqual(arrs, ['temp', 'ten', 'v'])
+
+    def test_add_array(self):
+        "Does the add_array method work."
+        dm = self.dm
+        data = self.data
+        pd = data.point_data
+        cd = data.cell_data
+
+        # Point data.
+        s = N.random.randn(12)
+        v = N.random.randn(12,3)
+        t = N.random.randn(12, 9)
+        dm.add_array(s, 'scalar')
+        sc = self.dm.point_scalars.keys()
+        sc.sort()
+        self.assertEqual(sc, ['p', 'scalar', 't'])
+        x = pd.get_array('scalar')
+        self.assertNotEqual(x, None)
+        dm.add_array(v, 'vector')
+        vc = self.dm.point_vectors.keys()
+        vc.sort()
+        self.assertEqual(vc, ['v', 'vector'])
+        x = pd.get_array('vector')
+        self.assertNotEqual(x, None)
+        dm.add_array(t, 'tensor')
+        vc = self.dm.point_tensors.keys()
+        vc.sort()
+        self.assertEqual(vc, ['ten', 'tensor'])
+        x = pd.get_array('tensor')
+        self.assertNotEqual(x, None)
+
+        # cell data.
+        s = N.random.randn(3)
+        v = N.random.randn(3, 3)
+        t = N.random.randn(3, 9)
+        dm.add_array(s, 'scalar', 'cell')
+        sc = self.dm.cell_scalars.keys()
+        sc.sort()
+        self.assertEqual(sc, ['scalar', 't'])
+        x = cd.get_array('scalar')
+        self.assertNotEqual(x, None)
+        dm.add_array(v, 'vector', 'cell')
+        vc = self.dm.cell_vectors.keys()
+        vc.sort()
+        self.assertEqual(vc, ['v', 'vector'])
+        x = cd.get_array('vector')
+        self.assertNotEqual(x, None)
+        dm.add_array(t, 'tensor', 'cell')
+        vc = self.dm.cell_tensors.keys()
+        vc.sort()
+        self.assertEqual(vc, ['ten', 'tensor'])
+        x = cd.get_array('tensor')
+        self.assertNotEqual(x, None)
+
+    def test_activate(self):
+        "Does activating a particular array work."
+        dm = self.dm
+        data = self.dm.output
+
+        dm.activate('p')
+        self.assertEqual(data.point_data.scalars.name, 'p')
+        dm.activate('t')
+        self.assertEqual(data.point_data.scalars.name, 't')
+
+        s = N.random.randn(3)
+        dm.add_array(s, 'foo', 'cell')
+        dm.activate('foo', 'cell')
+        self.assertEqual(data.cell_data.scalars.name, 'foo')
+        dm.activate('t', 'cell')
+        self.assertEqual(data.cell_data.scalars.name, 't')
+
+
+if __name__ == '__main__':
+    unittest.main()
