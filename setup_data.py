e64230b904487876a9c063fff04fd8c155542000
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-09-11 16:35:36 +0000

    * Changed project dependency versions to match the same versions planned for the ETS 3.0.2 release.

--- setup_data.py
+++ setup_data.py
@@ -14,13 +14,13 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.0.1')  # -- imports of persistence and resource in many places
-ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.1')    # The 'plugin' extra is required by loose-coupling in the mayavi ui plugin definition's default pespective.
-ENVISAGECORE = etsdep('EnvisageCore', '3.0.1')
-ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.0.1')
-TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.2')
-TRAITSGUI = etsdep('TraitsGUI', '3.0.2')
-TRAITS_UI = etsdep('Traits[ui]', '3.0.2')
+APPTOOLS = etsdep('AppTools', '3.0.0')  # -- imports of persistence and resource in many places
+ENTHOUGHTBASE = etsdep('EnthoughtBase', '3.0.0')    # The 'plugin' extra is required by loose-coupling in the mayavi ui plugin definition's default pespective.
+ENVISAGECORE = etsdep('EnvisageCore', '3.0.0')
+ENVISAGEPLUGINS = etsdep('EnvisagePlugins', '3.0.0')
+TRAITSBACKENDWX = etsdep('TraitsBackendWX', '3.0.1')
+TRAITSGUI = etsdep('TraitsGUI', '3.0.1')
+TRAITS_UI = etsdep('Traits[ui]', '3.0.1')
 
 
 # A dictionary of the pre_setup information.
