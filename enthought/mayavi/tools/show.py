afcf81cdebe048c2bda43d973d3d6e665267b80f
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-02-23 11:21:26 +0000

    ENH: Updating show so it supports a stop keyword argument that pops up a little UI that lets the user stop the mainloop temporarily and continue using Python.

--- enthought/mayavi/tools/show.py
+++ enthought/mayavi/tools/show.py
@@ -4,10 +4,14 @@
 
 from enthought.pyface.api import GUI, ApplicationWindow
 from enthought.etsconfig.api import ETSConfig
+from enthought.traits.api import HasTraits, Button, Any
+from enthought.traits.ui.api import View, Group, Item
 
 # Globals.
 # The GUI instance.
 _gui = None
+# The stop show instance.
+_stop_show = None
 
 def is_ui_running():
     """Returns True if the UI event loop is running."""
@@ -32,8 +36,40 @@ def is_ui_running():
 
     return False
 
+################################################################################
+# `StopShow` class.
+################################################################################ 
+class StopShow(HasTraits):
 
-def show(func=None):
+    ########################################
+    # Traits
+    stop = Button('Stop interaction', 
+                  desc='if the UI interaction is to be stopped')
+
+    # Private traits.
+    # Stores a reference to the UI object so it can be disposed when the
+    # interaction is stopped.
+    _ui = Any
+
+    view = View(Group(Item('stop'), show_labels=False),
+                buttons = [], title='Control Show')
+
+    ######################################################################
+    # `object` interface.
+    ###################################################################### 
+    def __init__(self, **traits):
+        super(StopShow, self).__init__(**traits)
+        self._ui = self.edit_traits()
+
+    ######################################################################
+    # Non-public interface.
+    ###################################################################### 
+    def _stop_fired(self):
+        _gui.stop_event_loop()
+        self._ui.dispose()
+
+
+def show(func=None, stop=False):
     """ Start interacting with the figure.
     
     By default, this function simply creates a GUI and starts its
@@ -45,12 +81,52 @@ def show(func=None):
     started and function is run in the toolkit's event loop.  The choice
     of UI is via `ETSConfig.toolkit`.  
 
+    If the argument stop is set to True then it pops up a UI where the
+    user can stop the event loop.  Subsequent calls to `show` will
+    restart the event loop.
+
+    **Parameters**
+
+     stop : bool -- Specifies if a UI dialog is displayed which allows
+                    the event loop to be stopped.
+
+    **Examples**
+
+    Here is a simple example demonstrating the use of show::
+
+      >>> from enthought.mayavi import mlab
+      >>> mlab.test_contour3d()
+      >>> mlab.show()
+
+    You can stop interaction via a simple pop up UI like so::
+
+      >>> mlab.test_contour3d()
+      >>> mlab.show(stop=True)
+
+    The decorator can be used like so::
+
+      >>> @mlab.show
+      ... def do():
+      ...    mlab.test_contour3d()
+      ...
+      >>> do()
+    
+    The decorator can also be passed the stop argument::
+
+      >>> @mlab.show(stop=True)
+      ... def do():
+      ...    mlab.test_contour3d()
+      ...
+      >>> do()
+
     """
-    global _gui
+    global _gui, _stop_show
     if func is None:
         if not is_ui_running():
             g = GUI()
             _gui = g
+            if stop:
+                _stop_show = StopShow()
             g.start_event_loop()
         return
 
@@ -58,10 +134,12 @@ def show(func=None):
         """Wrapper function to run given function inside the GUI event
         loop.
         """
-        global _gui
+        global _gui, _stop_show
         tk = ETSConfig.toolkit
 
         if is_ui_running():
+            # In this case we should not pop up the UI since we likely
+            # don't want to stop the mainloop.
             return func(*args, **kw)
         else:
             g = GUI()
@@ -73,6 +151,9 @@ def show(func=None):
 
             GUI.invoke_later(func, *args, **kw)
             _gui = g
+            if stop:
+                # Pop up the UI to stop the mainloop.
+                _stop_show = StopShow()
             g.start_event_loop()
 
     return wrapper
