69f8fd4ca85e13442f8c3dc1d6277f3d77daa2ee
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-09 03:52:10 +0000

    BUG: The mlab.test_quiver3d() function was producing nans which has been fixed.

--- enthought/mayavi/tools/helper_functions.py
+++ enthought/mayavi/tools/helper_functions.py
@@ -251,8 +251,8 @@ quiver3d = document_pipeline(Quiver3D())
 def test_quiver3d():
     x, y, z = numpy.mgrid[-2:3, -2:3, -2:3]
     r = numpy.sqrt(x**2 + y**2 + z**4)
-    u = y*numpy.sin(r)/r
-    v = -x*numpy.sin(r)/r
+    u = y*numpy.sin(r)/(r+0.001)
+    v = -x*numpy.sin(r)/(r+0.001)
     w = numpy.zeros_like(z)
     obj = quiver3d(x, y, z, u, v, w, line_width=3, scale_factor=1)
     return obj
