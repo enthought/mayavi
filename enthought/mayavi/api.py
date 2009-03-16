df161db903bd52623d1848fc8e2729d8eac4b74d
prabhu <prabhu@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-16 10:28:18 +0000

    TEST: Renaming runtests to test to be consistent with scipy/numpy etal.

--- enthought/mayavi/api.py
+++ enthought/mayavi/api.py
@@ -1,4 +1,4 @@
 from enthought.mayavi.version import version, version as __version__
 from enthought.mayavi.core.engine import Engine
 from enthought.mayavi.core.off_screen_engine import OffScreenEngine
-from enthought.mayavi.tests.runtests import m2_tests as runtests
+from enthought.mayavi.tests.runtests import m2_tests as test
