37c151de6b2241e68f7287349b43f6dce1150093
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-04-27 05:29:01 +0000

    API: add an API module for core.ui

---
+++ enthought/mayavi/core/ui/api.py
@@ -0,0 +1,6 @@
+from enthought.mayavi.tools.mlab_scene_model import MlabSceneModel
+from enthought.mayavi.core.ui.mayavi_scene import MayaviScene
+from enthought.tvtk.pyface.scene_editor import SceneEditor
+from enthought.mayavi.core.ui.engine_view import EngineView
+from enthought.mayavi.core.ui.engine_rich_view import EngineRichView
+
