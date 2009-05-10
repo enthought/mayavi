cc7bcf1972f64e909a50046255c2498f45de84fe
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-05-10 13:50:32 +0000

    API: Add PipelineBase to enthought.mayavi.core.api as it is needed for the interactive examples

--- enthought/mayavi/core/api.py
+++ enthought/mayavi/core/api.py
@@ -2,6 +2,7 @@
 from registry import registry
 from metadata import SourceMetadata
 from pipeline_info import PipelineInfo
+from pipeline_base import PipelineBase
 from engine import Engine
 from null_engine import NullEngine
 from module_manager import ModuleManager
