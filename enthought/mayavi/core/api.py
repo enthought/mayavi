71137b4fde0ef746b74591f5959ef87a77278d3c
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-07-05 23:36:18 +0000

    MISC: Use local imports

--- enthought/mayavi/core/api.py
+++ enthought/mayavi/core/api.py
@@ -1,9 +1,13 @@
 
-from registry import registry
-from metadata import SourceMetadata
-from pipeline_info import PipelineInfo
-from pipeline_base import PipelineBase
-from engine import Engine
-from null_engine import NullEngine
-from module_manager import ModuleManager
+from .registry import registry
+from .metadata import SourceMetadata
+from .pipeline_info import PipelineInfo
+from .pipeline_base import PipelineBase
+from .engine import Engine
+from .null_engine import NullEngine
+from .module_manager import ModuleManager
+from .source import Source
+from .scene import Scene
+from .filter import Filter
+from .module import Module
 
