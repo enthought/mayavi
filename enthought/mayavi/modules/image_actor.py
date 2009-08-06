572e85ed87c8780c0b375d57af91337be049ffe2
Swarts <Swarts@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-08-06 14:13:30 +0000

    Modified ImageActor to use ImageChangeInformation filter to preserve the spacing and origin from the source image. The ImageMapToColors filter does not always copy these to the output image.

--- enthought/mayavi/modules/image_actor.py
+++ enthought/mayavi/modules/image_actor.py
@@ -33,6 +33,10 @@ class ImageActor(Module):
     image_map_to_color = Instance(tvtk.ImageMapToColors, (),
                                             allow_none=False, record=True)
 
+    # The vtkImageMapToColors doesn't always preserve spacing, so we need to 
+    # copy the information from the source.
+    copy_information = Instance(tvtk.ImageChangeInformation, ())
+
     map_scalars_to_color = Bool
 
     _force_map_scalars_to_color = Property(depends_on='module_manager.source')
@@ -75,7 +79,10 @@ class ImageActor(Module):
         if self.map_scalars_to_color: 
             self.image_map_to_color.input = src.outputs[0]
             self.image_map_to_color.lookup_table = mm.scalar_lut_manager.lut
-            self.actor.input = self.image_map_to_color.output
+            # Copy the information (spacing, etc) from the original image
+            self.copy_information.information_input = src.outputs[0]
+            self.copy_information.input = self.image_map_to_color.output
+            self.actor.input = self.copy_information.output
         else:
             self.actor.input = src.outputs[0]
         self.pipeline_changed = True
