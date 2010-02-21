e56ec362e22a58a01696d10992c084015d1e5fc4
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-02-21 19:35:48 +0000

    MISC: Clean up unused import in example.

--- examples/mayavi/advanced_visualization/datasets.py
+++ examples/mayavi/advanced_visualization/datasets.py
@@ -34,6 +34,7 @@ The following images are created:
 from numpy import array, random, linspace, pi, ravel, cos, sin, empty 
 from enthought.tvtk.api import tvtk
 
+from enthought.mayavi.scripts import mayavi2
 from enthought.mayavi.sources.vtk_data_source import VTKDataSource
 
 from enthought.mayavi import mlab
