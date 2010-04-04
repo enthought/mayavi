225dd369e51ede546dc683a2ab1f5993d4cf7b48
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-04-04 18:44:28 +0000

    DOC: Improve slightly the docstring of the atomic_orbital example.

--- examples/mayavi/mlab/atomic_orbital.py
+++ examples/mayavi/mlab/atomic_orbital.py
@@ -7,13 +7,17 @@ a second data set on the output of the filter. Here we use the contour
 filter to extract isosurfaces of the norm of a complex field, and we
 display the phase of the field with the colormap.
 
-The first step is to create a data source with two scalar datasets. The
-second step is to apply filters and modules, using the
-'set_active_attribute' filter to select on which data these
-apply.
-
 The field we choose to plot is a simplified version of the 3P_y atomic
 orbital for hydrogen-like atoms.
+
+The first step is to create a data source with two scalar datasets. The
+second step is to apply filters and modules, using the
+'set_active_attribute' filter to select on which data these apply.
+
+Creating a data source with two scalar datasets is actually slighlty
+tricky, as it requires some understanding of the layout of the datasets
+in TVTK. The reader is referred to :ref:`data-structures-used-by-mayavi`
+for more details.
 """
 # Author: Gael Varoquaux <gael.varoquaux@normalesup.org> 
 # Copyright (c) 2008, Enthought, Inc.
