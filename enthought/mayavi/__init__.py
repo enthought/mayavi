01a644dab1770b06afd0d65a523eed4e37ada046
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-05-09 18:09:09 +0000

    ENH: Try to push the use of wx > 2.8 in the __init__ of Mayavi: this is required for the autodoc features of Sphinx: without out this, on systems with wx 2.6 installed, Mayavi doesn't import out of the box.

--- enthought/mayavi/__init__.py
+++ enthought/mayavi/__init__.py
@@ -1,6 +1,20 @@
-# Author: Prabhu Ramachandran
-# Copyright (c) 2004, Enthought, Inc.
+# Author: Prabhu Ramachandran, Gael Varoquaux
+# Copyright (c) 2004-2010, Enthought, Inc.
 # License: BSD Style.
 """ A tool for easy and interactive visualization of data.
     Part of the Mayavi project of the Enthought Tool Suite.
 """
+
+# Try forcing the use of wx 2.8 before any other import.
+import sys
+if not 'wx' in sys.modules:
+    try:
+        # Try forcing the use of wx 2.8
+        from enthought.etsconfig.api import ETSConfig
+        if ETSConfig.toolkit in ('wx', ''):
+            import wxversion
+            wxversion.ensureMinimal('2.8')
+    except ImportError:
+        """ wxversion not installed """
+
+
