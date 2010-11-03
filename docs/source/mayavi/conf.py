7abf2df8366ec94684484b696f2ce90c4afeb704
ascopatz <ascopatz@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-11-03 22:52:24 +0000

    Updated documentation directory tree to a more modern paradigm.

--- docs/source/mayavi/conf.py
+++ docs/source/mayavi/conf.py
@@ -50,7 +50,7 @@ extensions = ['sphinx.ext.autodoc', 'traitsdoc']
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['.templates']
+templates_path = ['_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -105,7 +105,7 @@ html_style = 'default.css'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['.static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
