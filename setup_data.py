4a95e49570a26f1295a2dad9d6d71ad9a887f1b1
gvaroquaux <gvaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2011-01-26 23:27:39 +0000

    MISC: Add back the extra requires for the [app]

--- setup_data.py
+++ setup_data.py
@@ -1,8 +1,16 @@
 INFO = {
+    'extras_require': {
+        'app' : [
+            'EnvisageCore >= 3.2.0.dev',
+            'EnvisagePlugins >= 3.2.0.dev',
+            'TraitsBackendWX >= 3.6.0.dev',
+            ],
+        },
     'name': 'Mayavi',
     'version': '3.4.1',
     'install_requires': [
         'AppTools >= 3.4.1.dev',
         'Traits >= 3.6.0.dev',
+        'TraitsGUI >= 3.6.0.dev',
     ],
 }
