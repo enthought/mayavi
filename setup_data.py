f21558b7bb92519154d6bafd82786166791b21f2
ischnell <ischnell@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2011-01-27 04:39:00 +0000

    cleanup

--- setup_data.py
+++ setup_data.py
@@ -1,11 +1,4 @@
 INFO = {
-    'extras_require': {
-        'app' : [
-            'EnvisageCore >= 3.2.0.dev',
-            'EnvisagePlugins >= 3.2.0.dev',
-            'TraitsBackendWX >= 3.6.0.dev',
-            ],
-        },
     'name': 'Mayavi',
     'version': '3.4.1',
     'install_requires': [
@@ -13,4 +6,11 @@ INFO = {
         'Traits >= 3.6.0.dev',
         'TraitsGUI >= 3.6.0.dev',
     ],
+    'extras_require': {
+        'app': [
+            'EnvisageCore >= 3.2.0.dev',
+            'EnvisagePlugins >= 3.2.0.dev',
+            'TraitsBackendWX >= 3.6.0.dev',
+        ],
+    },
 }
