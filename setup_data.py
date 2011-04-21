47da856c1060dae49579fcdfb548c1799b09c2bd
Ilan Schnell <ischnell@enthought.com>
2011-04-21 01:56:31 -0500

    update dependencies

--- setup_data.py
+++ setup_data.py
@@ -3,14 +3,13 @@ INFO = {
     'version': '3.4.2',
     'install_requires': [
         'AppTools >= 3.4.1.dev',
-        'Traits >= 3.6.0.dev',
-        'TraitsGUI >= 3.6.0.dev',
+        'traits >= 3.6.0.dev',
+        'traitsui >= 3.6.0.dev',
     ],
     'extras_require': {
         'app': [
             'EnvisageCore >= 3.2.0.dev',
             'EnvisagePlugins >= 3.2.0.dev',
-            'TraitsBackendWX >= 3.6.0.dev',
         ],
     },
 }
