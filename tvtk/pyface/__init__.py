47971fd29335c4f8353af7f513d8c2e749431842
Ilan Schnell <ischnell@enthought.com>
2011-06-07 22:17:08 -0500

    DOC: update and cleanup __init__.py

--- tvtk/pyface/__init__.py
+++ tvtk/pyface/__init__.py
@@ -1,22 +1,10 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2005, Enthought, Inc.
+# Copyright (c) 2005-2011, Enthought, Inc.
 # All rights reserved.
 #
 # This software is provided without warranty under the terms of the BSD
-# license included in enthought/LICENSE.txt and may be redistributed only
+# license included in LICENSE.txt and may be redistributed only
 # under the conditions described in the aforementioned license.  The license
 # is also available online at http://www.enthought.com/licenses/BSD.txt
 # Thanks for using Enthought open source!
 #
 # Author: Enthought, Inc.
-# Description: <Enthought pyface package component>
-#------------------------------------------------------------------------------
-__import__('pkg_resources').declare_namespace(__name__)
-
-# For py2app / py2exe support
-try:
-    import modulefinder
-    for p in __path__:
-        modulefinder.AddPackagePath(__name__, p)
-except:
-    pass
