9a58c7239539073f6cb72aa92691c8b4bf7c3590
Deepak Surti <dsurti@enthought.com>
2013-12-17 18:55:17 +0530

    Fix failing test case when only package is imported

--- mayavi/core/customize.py
+++ mayavi/core/customize.py
@@ -75,10 +75,17 @@ def _import_contrib(pkg):
     mod = None
     try:
         components = pkg.split('.')
-        mod_name = '.'.join(components[:-1])
-        sym_name = components[-1]
-        m = __import__(mod_name, globals(), locals(), [sym_name], level=0)
-        mod = getattr(m, sym_name)
+        mod_name = None
+        sym_name = None
+        if len(components) > 1:
+            mod_name = '.'.join(components[:-1])
+            sym_name = components[-1]
+            mod = __import__(mod_name, globals(), locals(), [sym_name], level=0)
+        else:
+            mod_name = components[0]
+            mod = __import__(mod_name, globals(), locals(), [mod_name], level=0)
+        if sym_name:
+            mod = getattr(mod, sym_name)
     except Exception:
         print "*"*80
         traceback.print_exc(file=sys.stdout)
