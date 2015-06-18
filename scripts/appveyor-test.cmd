74c4dffecf6579cd40ff5144c0d9a3d495c7813f
Ioannis Tziakos <ioannist@enthought.com>
2015-06-18 17:02:39 +0100

    Update appveyor-test.cmd

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -1,3 +1,12 @@
 "%sdkver%" -q -version:v7.0
 call setenv /x64
-python setup.py test
+coverage erase
+coverage run -p -m nose.core -v tvtk/tests
+coverage run -p -m nose.core -v mayavi
+# Run the integration tests.
+cd integrationtests/mayavi
+coverage run --rcfile=../../.coveragerc -p run.py
+cd ../../
+cp integrationtests/mayavi/.coverage.* ./
+coverage combine
+coverage report
