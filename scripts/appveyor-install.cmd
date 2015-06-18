d5c4e0401d8e284f3044e78cebdde89112dbcc28
Ioannis Tziakos <ioannist@enthought.com>
2015-06-18 17:01:47 +0100

    Update appveyor-install.cmd

--- scripts/appveyor-install.cmd
+++ scripts/appveyor-install.cmd
@@ -1,4 +1,12 @@
 "%sdkver%" -q -version:v7.0
 call setenv /x64
-if %cython%==1 pip install cython
-pip install -r requirements.txt
+pip install nose
+pip install Sphinx
+pip install coverage
+# Test against the current master of traits, traitsui and apptools
+pip install git+http://github.com/enthought/traits.git#egg=traits
+pip install git+http://github.com/enthought/traitsui.git#egg=traitsui
+pip install git+http://github.com/enthought/apptools.git#egg=apptools
+pip install git+http://github.com/enthought/pyface.git#egg=pyface
+pip install git+http://github.com/enthought/envisage.git#egg=envisage
+python setup.py develop
