8f541cbbe39ef6c39216cec323be0b385e20c406
Ioannis Tziakos <ioannist@enthought.com>
2015-06-18 23:03:24 +0100

    remove #

--- scripts/appveyor-test.cmd
+++ scripts/appveyor-test.cmd
@@ -3,10 +3,10 @@ call setenv /x64
 coverage erase
 coverage run -p -m nose.core -v tvtk/tests
 coverage run -p -m nose.core -v mayavi
-# Run the integration tests.
 cd integrationtests/mayavi
 coverage run --rcfile=../../.coveragerc -p run.py
 cd ../../
-cp integrationtests/mayavi/.coverage.* ./
+copy integrationtests/mayavi/.coverage.* ./
 coverage combine
 coverage report
+b
