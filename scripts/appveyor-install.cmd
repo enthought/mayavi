c405b83dc1a82cde7f3ed90d0bbb2b82f0ef8e8f
Ioannis Tziakos <ioannist@enthought.com>
2015-06-18 18:50:34 +0100

    use the requirements file and redirect cache-dir

--- scripts/appveyor-install.cmd
+++ scripts/appveyor-install.cmd
@@ -1,14 +1,4 @@
 "%sdkver%" -q -version:v7.0
 call setenv /x64
-pip install nose
-pip install Sphinx
-pip install coverage
-pip install numpy
-pip install pyside
-# Test against the current master of traits, traitsui and apptools
-pip install git+http://github.com/enthought/traits.git#egg=traits
-pip install git+http://github.com/enthought/traitsui.git#egg=traitsui
-pip install git+http://github.com/enthought/apptools.git#egg=apptools
-pip install git+http://github.com/enthought/pyface.git#egg=pyface
-pip install git+http://github.com/enthought/envisage.git#egg=envisage
+pip install --cache-dir c:/tmp -r appveyor-requirements.txt
 python setup.py develop
