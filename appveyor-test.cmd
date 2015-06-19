99dfed910fb7964bbaddc4f8f3ba8a748eb9cacf
Ioannis Tziakos <ioannist@enthought.com>
2015-06-19 10:55:21 +0100

    move files to the main folder

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -3,9 +3,5 @@ call setenv /x64
 coverage erase
 coverage run -p -m nose.core -v tvtk/tests
 coverage run -p -m nose.core -v mayavi
-rem cd integrationtests/mayavi
-rem coverage run --rcfile=../../.coveragerc -p run.py
-rem cd ../../
-rem copy integrationtests/mayavi/.coverage.* ./
 coverage combine
 coverage report
