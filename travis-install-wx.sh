bd32741ed5338bb324cac8d1425cfa32e65ea227
itziakos <ioannist@enthought.com>
2015-06-30 19:32:25 +0100

    use caching and install wx only when necessary

--- travis-install-wx.sh
+++ travis-install-wx.sh
@@ -1,10 +1,11 @@
 set -e
+rm -Rf wxPython-src-2.8.12.1
 wget http://sourceforge.net/projects/wxpython/files/wxPython/2.8.12.1/wxPython-src-2.8.12.1.tar.bz2/download
 tar -xf download
 cd wxPython-src-2.8.12.1
 sed -i '/SEARCH_LIB=/a SEARCH_LIB=${SEARCH_LIB}" \/usr\/lib\/x86_64-linux-gnu"' ./configure
-mkdir wxGTK
-cd wxGTK
+mkdir wxGTK-build
+cd wxGTK-build
 ../configure --enable-unicode --prefix=$VIRTUAL_ENV --enable-debug_flag --enable-optimize --with-opengl
 make -j 3
 make -C contrib/src/stc -j 3
