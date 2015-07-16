8bf2dd1d3a09f2c8f5c5c53bc4e19d946119fc92
Deepak Surti <dsurti@enthought.com>
2015-07-16 21:58:39 +0530

    Increase the number of force allocated objects.
    This test is inconsistent. Hopefully increasing the number resolves it.

--- tvtk/tests/test_tvtk.py
+++ tvtk/tests/test_tvtk.py
@@ -200,7 +200,7 @@ class TestTVTK(unittest.TestCase):
         # The test sometimes fails as VTK seems to generate objects with the
         # same memory address and hash, we try to force it to allocate more
         # objects so as to not end up reusing the same address and hash.
-        junk = [vtk.vtkConeSource() for i in range(5)]
+        junk = [vtk.vtkConeSource() for i in range(50)]
 
         # Now get another ConeSource and ensure the hash is different.
         cs = tvtk.ConeSource()
