251ea45e22e57fdfb1051bdd251b367451f56fc0
itziakos <ioannist@enthought.com>
2015-07-17 18:41:41 +0100

    try to cache the last part of the wxPython build

--- travis-install-wx.sh
+++ travis-install-wx.sh
@@ -13,12 +13,20 @@ make -C contrib/src/stc -j 3
 make -C contrib/src/stc install
 make -C contrib/src/gizmos -j 3
 make -C contrib/src/gizmos install
-cd ../wxPython
-svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/ wx/lib/agw
-svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/FloatCanvas/floatcanvas/ wx/lib/floatcanvas
-svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/PubSub/pubsub/ wx/lib/pubsub
-mkdir wx/lib/pubsub/pubsub1
-mkdir wx/lib/pubsub/pubsub2
-svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/XRCed/ wx/tools/XRCed
-svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/Editra/ wx/tools/Editra
-python setup.py install
+if [ -f $HOME/.cache/wxPython-2.8.12.1-cp27-none-linux_x86_64.whl ]; then
+   echo "wxPython wheel found"
+else
+   echo "Building wxPython wheel"
+   cd ../wxPython
+   svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/AGW/agw/ wx/lib/agw
+   svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/FloatCanvas/floatcanvas/ wx/lib/floatcanvas
+   svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/PubSub/pubsub/ wx/lib/pubsub
+   mkdir wx/lib/pubsub/pubsub1
+   mkdir wx/lib/pubsub/pubsub2
+   svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/XRCed/ wx/tools/XRCed
+   svn co http://svn.wxwidgets.org/svn/wx/wxPython/3rdParty/Editra/ wx/tools/Editra
+   python setup.py bdist_egg
+   wheel convert ./dist/wxPython-2.8.12.1-py2.7-linux-x86_64.egg $HOME/.cache/
+fi
+
+pip install $HOME/.cache/wxPython-2.8.12.1-cp27-none-linux_x86_64.whl
