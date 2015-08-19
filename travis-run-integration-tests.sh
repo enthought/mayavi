7d0c06db60ee95ece22ce95f9cc0dead128dfad4
Ioannis Tziakos <ioannist@enthought.com>
2015-08-19 15:07:09 +0100

    run 2to3 conversion on the integration tests when necessary

--- travis-run-integration-tests.sh
+++ travis-run-integration-tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
-cd integrationtests/mayavi
+cd ../integrationtests/mayavi
+if [ ${TRAVIS_PYTHON_VERSION} == "3.4"  ]; then
+    python "${TRAVIS_PYTHON_VERSION}/Tools/Script/2to3.py" -w ./
+fi
 coverage run --rcfile=../../.coveragerc -p run.py
 cd ../..
 cp integrationtests/mayavi/.coverage.* ./
