26e5ccd8a59ed8c3ceeb45e14ce2d5c029144a1a
enthought <enthought@simphony-base.(none)>
2015-08-20 18:29:00 +0100

    fix the 2to3 call for updating the integration tests

--- travis-run-integration-tests.sh
+++ travis-run-integration-tests.sh
@@ -2,7 +2,7 @@
 set -e
 cd ../integrationtests/mayavi
 if [ ${TRAVIS_PYTHON_VERSION} == "3.4"  ]; then
-    python "${TRAVIS_PYTHON_VERSION}/Tools/Script/2to3.py" -w ./
+    python -m lib2to3.py -w ./
 fi
 coverage run --rcfile=../../.coveragerc -p run.py
 cd ../..
