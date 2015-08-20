d2b4e94e109a5146c323d1ebce2117fa74dcd141
enthought <enthought@simphony-base.(none)>
2015-08-20 18:33:51 +0100

    this should be better

--- travis-run-integration-tests.sh
+++ travis-run-integration-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 cd ../integrationtests/mayavi
-if [ ${TRAVIS_PYTHON_VERSION} == "3.4"  ]; then
+if [[ ${TRAVIS_PYTHON_VERSION} == "3.4"  ]]; then
     python -m lib2to3.py -w ./
 fi
 coverage run --rcfile=../../.coveragerc -p run.py
