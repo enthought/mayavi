05fb026ce5154abbe95b4f6273f1112192fd9d6e
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2015-08-22 15:56:50 +0530

    Fix tests on Python2.

--- mayavi/sources/vtk_data_source.py
+++ mayavi/sources/vtk_data_source.py
@@ -6,6 +6,7 @@ gzipped string.
 # Copyright (c) 2005-2015, Enthought, Inc.
 # License: BSD Style.
 
+import sys
 import os
 import tempfile
 
@@ -171,14 +172,20 @@ class VTKDataSource(Source):
         data = self.data
         if data is not None:
             sdata = write_dataset_to_string(data)
-            z = gzip_string(sdata.encode('ascii'))
+            if sys.version_info[0] > 2:
+                z = gzip_string(sdata.encode('ascii'))
+            else:
+                z = gzip_string(sdata)
             d['data'] = z
         return d
 
     def __set_pure_state__(self, state):
         z = state.data
         if z is not None:
-            d = gunzip_string(z).decode('ascii')
+            if sys.version_info[0] > 2:
+                d = gunzip_string(z).decode('ascii')
+            else:
+                d = gunzip_string(z)
             r = tvtk.DataSetReader(read_from_input_string=1,
                                    input_string=d)
             warn = r.global_warning_display
