9c03b68c136c05db09b89c79b72f27c2e6400fa7
Kit Yan Choi <kitchoi@users.noreply.github.com>
2016-04-19 13:53:32 +0100

    the skip reason was wrong
    The purpose of this test was to make sure that failure in unpickling pylab_luts.pkl does not prevent lut_manager from loading.

--- mayavi/tests/test_pylab_luts.py
+++ mayavi/tests/test_pylab_luts.py
@@ -8,10 +8,10 @@ class TestPylabLuts(unittest.TestCase):
 
     @unittest.skipIf("mayavi.core.lut_manager" in sys.modules,
                      ("mayavi.core.lut_manager is already loaded in "
-                      "another tests. If those tests pass, this test "
-                      "is automatically fulfilled"))
+                      "another tests. Can't run this test."))
     @patch("mayavi.core.lut.__file__", "wrong_path/for_lut.py")
     def test_fail_load_pylab_luts(self):
         """ Test if lut_manager can be loaded despite faulty pylab_luts.pkl
         """
-        import mayavi.core.lut_manager
+        from mayavi.core.lut_manager import pylab_luts
+        self.assertEqual(pylab_luts, {})
