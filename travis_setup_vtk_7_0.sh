45c80daa4083253a75829ae23df3217683f7bef1
Kit Yan Choi <kchoi@enthought.com>
2016-04-20 11:50:20 +0100

    clean shell script for setting up vtk 6.3 and 7.0

--- travis_setup_vtk_7_0.sh
+++ travis_setup_vtk_7_0.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+# This is the name of the directory after unpacking
 VTK_PYTHON=vtkpython-7.0.0-Linux-64bit
-FILE_PREFIX=vtkpython-7.0.0-Linux-64bit
-DOWNLOAD_LINK=http://www.vtk.org/files/release/7.0/${FILE_PREFIX}.tar.gz
+
+# Download link to the vtkPython
+DOWNLOAD_LINK=http://www.vtk.org/files/release/7.0/vtkpython-7.0.0-Linux-64bit.tar.gz
+
+# Cache directory
 CACHE_DIR=${HOME}/.cache
 
-CURRENT_DIR=$PWD
-cd $CACHE_DIR
-
-if [ -d "${VTK_PYTHON}" ]; then
+# Locate the unpacked directory, or unpack if it wasn't done already
+if [ -d "${CACHE_DIR}/${VTK_PYTHON}" ]; then
     echo "${VTK_PYTHON} is found"
 else
     echo "Downloading ${VTK_PYTHON}"
-    wget ${DOWNLOAD_LINK}
-    gunzip ${FILE_PREFIX}.tar.gz
-    tar -xf ${FILE_PREFIX}.tar
+    wget ${DOWNLOAD_LINK} -O vtk_python.tar.gz
+    gunzip vtk_python.tar.gz
+    tar -xf vtk_python.tar -C ${CACHE_DIR}
+    rm -f vtk_python.tar
 fi
 
 export PYTHONPATH=${CACHE_DIR}/${VTK_PYTHON}/lib/python2.7/site-packages
 export LD_LIBRARY_PATH=${CACHE_DIR}/${VTK_PYTHON}/lib
-cd $CURRENT_DIR
 
