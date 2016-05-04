6ede27d1cfbaa3b374cfd338d0dbb8826110b293
Stefano Borini <stefanoborini@users.noreply.github.com>
2016-05-04 10:34:00 +0100

    Added comments to restart codecov, again.

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -2,11 +2,14 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update         
     brew tap homebrew/science
     brew tap homebrew/python
+    
+    # Install needed packages
     brew install python
     brew install pyqt
     brew install wxpython
     brew install numpy
 
+    # Print out python and version
     which python
     python --version
 else
