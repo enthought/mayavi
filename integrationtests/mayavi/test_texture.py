6c87fda38c1d8dd3074443cc26b897b31c9f4396
Kit Choi <kchoi@enthought.com>
2016-06-28 20:13:11 +0100

    Change texture to its inverse and compare images

--- integrationtests/mayavi/test_texture.py
+++ integrationtests/mayavi/test_texture.py
@@ -9,7 +9,6 @@ from PIL import Image
 
 from tvtk.api import tvtk
 from mayavi import mlab
-from mayavi.tools.figure import savefig
 
 from common import TestCase
 
@@ -22,7 +21,8 @@ class TestTextureUnitTest(unittest.TestCase):
     def setUp(self):
         # Make a temporary directory for saved figures
         self.temp_dir = tempfile.mkdtemp()
-        self.filename = os.path.join(self.temp_dir, "saved_figure.png")
+        self.filename1 = os.path.join(self.temp_dir, "saved_figure.png")
+        self.filename2 = os.path.join(self.temp_dir, "inverted.png")
 
         # this ensures that the temporary directory is removed
         self.addCleanup(self.remove_tempdir)
@@ -31,9 +31,18 @@ class TestTextureUnitTest(unittest.TestCase):
         # the image is a black-white checker box pattern
         image_path = os.path.join(
             MAYAVI_DIR, "../integrationtests/mayavi/images/checker.jpg")
-        img=tvtk.JPEGReader(file_name=image_path)
-        self.texture=tvtk.Texture(input_connection=img.output_port,
-                                  interpolate=1)
+        img = tvtk.JPEGReader(file_name=image_path)
+        self.texture = tvtk.Texture(input_connection=img.output_port,
+                                    interpolate=1)
+
+        # inverted version of the same image
+        image_path = os.path.join(
+            MAYAVI_DIR,
+            "../integrationtests/mayavi/images/checker_inverted.jpg")
+        img = tvtk.JPEGReader(file_name=image_path)
+        self.texture_inverted = tvtk.Texture(
+            input_connection=img.output_port,
+            interpolate=1)
 
     def remove_tempdir(self):
         shutil.rmtree(self.temp_dir)
@@ -43,15 +52,15 @@ class TestTextureUnitTest(unittest.TestCase):
 
     def add_texture(self, source, mode):
         # Add texture
-        source.actor.actor.mapper.scalar_visibility=False
-        source.actor.enable_texture=True
-        source.actor.tcoord_generator_mode=mode
-        source.actor.actor.texture=self.texture
+        source.actor.actor.mapper.scalar_visibility = False
+        source.actor.enable_texture = True
+        source.actor.tcoord_generator_mode = mode
+        source.actor.actor.texture = self.texture
 
     def test_texture_curve(self):
         """ Test texture on mlab.surf """
         mlab.figure()
-        X, Y = numpy.mgrid[-1:1:20j,-1:1:20j]
+        X, Y = numpy.mgrid[-1:1:20j, -1:1:20j]
         Z = -numpy.cos(Y*X)+.5
         source = mlab.surf(X, Y, Z, color=(1., 1., 1.))
 
@@ -64,10 +73,13 @@ class TestTextureUnitTest(unittest.TestCase):
         # Zoom in closer for analysis
         mlab.view(67.2, 47, 2.1, [0.22, 0.13, -0.6])
 
-        mlab.savefig(self.filename, size=(400, 300))
+        mlab.savefig(self.filename1, size=(400, 300))
 
-        # Check the saved image (if texture fails, std ~ 10)
-        self.check_image_std(target_std=150.)
+        # Change the texture to the inverted one
+        source.actor.actor.texture = self.texture_inverted
+        mlab.savefig(self.filename2, size=(400, 300))
+
+        self.check_images_differ(self.filename1, self.filename2)
 
     def test_texture_sphere(self):
         """ Test texture on mlab.points3d (sphere) """
@@ -83,10 +95,13 @@ class TestTextureUnitTest(unittest.TestCase):
         # Zoom in closer for analysis
         mlab.view(-158., 10.4,  2., [0, 0, 0])
 
-        mlab.savefig(self.filename, size=(400, 300))
+        mlab.savefig(self.filename1, size=(400, 300))
 
-        # Check the saved image (if texture fails, std ~ 90)
-        self.check_image_std(target_std=150.)
+        # Change the texture to the inverted one
+        source.actor.actor.texture = self.texture_inverted
+        mlab.savefig(self.filename2, size=(400, 300))
+
+        self.check_images_differ(self.filename1, self.filename2)
 
     def test_texture_cylinder(self):
         """ Test texture on mlab.points3d (cylinder) """
@@ -102,21 +117,20 @@ class TestTextureUnitTest(unittest.TestCase):
         # Zoom in closer for analysis
         mlab.view(52., 38., 1.4, [0., 0., 0.])
 
-        mlab.savefig(self.filename, size=(400, 300))
+        mlab.savefig(self.filename1, size=(400, 300))
 
-        # Check the saved image (if texture fails, std ~ 90)
-        self.check_image_std(target_std=150.)
+        # Change the texture to the inverted one
+        source.actor.actor.texture = self.texture_inverted
+        mlab.savefig(self.filename2, size=(400, 300))
 
-    def check_image_std(self, target_std):
-        # Check that the pixels in the image vary greatly as
-        # the pattern is a checker box
-        image = numpy.array(Image.open(self.filename))[:, :, :3].sum(axis=2)
-        message = ("The Texture is a checker box but the image "
-                   "pixels do not vary greatly enough. "
-                   "It looks wrong. Target standard deviation: {0}, "
-                   "got {1}")
-        self.assertGreaterEqual(image.std(), target_std,
-                                message.format(target_std, image.std()))
+        self.check_images_differ(self.filename1, self.filename2)
+
+    def check_images_differ(self, image_file1, image_file2):
+        image1 = numpy.array(Image.open(image_file1))[:, :, :3].sum(axis=2)
+        image2 = numpy.array(Image.open(image_file2))[:, :, :3].sum(axis=2)
+        self.assertGreaterEqual((image1 - image2).std(), image1.std(),
+                                "The texture is inverted but the two images"
+                                "do not differ greatly.  Looks wrong.")
 
 
 class TestTexture(TestCase):
