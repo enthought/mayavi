9c17a7da30f796008216b67876be381ebb5e48aa
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2018-03-10 14:10:28 +0530

    Try get builds working on Travis.
    Also fix a few mistakes in the appveyor.yml script.

---
+++ ci/install-edm-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+install_edm() {
+    local EDM_MAJOR_MINOR="$(echo "$INSTALL_EDM_VERSION" | sed -E -e 's/([[:digit:]]+\.[[:digit:]]+)\..*/\1/')"
+    local EDM_PACKAGE="edm_${INSTALL_EDM_VERSION}_linux_x86_64.sh"
+    local EDM_INSTALLER_PATH="${HOME}/.cache/download/${EDM_PACKAGE}"
+
+    if [ ! -e "$EDM_INSTALLER_PATH" ]; then
+        curl -o "$EDM_INSTALLER_PATH" -L "https://package-data.enthought.com/edm/rh5_x86_64/${EDM_MAJOR_MINOR}/${EDM_PACKAGE}"
+    fi
+
+    bash "$EDM_INSTALLER_PATH" -b -p "${HOME}/edm"
+}
+
+install_edm
--- travis/travis-run-integration-tests.sh
+++ travis/travis-run-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 cd integrationtests/mayavi
-coverage run --rcfile=../../.coveragerc -p run.py
+edm run -- coverage run --rcfile=../../.coveragerc -p run.py
 cd ../..
 cp integrationtests/mayavi/.coverage.* ./
