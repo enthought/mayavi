5b93d9e643d146a8f754847082303081d29fa4c9
amalss18 <amalssebastian@gmail.com>
2019-07-01 16:12:14 +0530

    made requested changes

--- examples/mayavi/data_interaction/normal_flipping_stl.py
+++ examples/mayavi/data_interaction/normal_flipping_stl.py
@@ -7,99 +7,6 @@ from tvtk.api import tvtk
 from tvtk.common import configure_input
 import numpy as np
 from mayavi import mlab
-import tempfile
-
-cube_stl = """solid cube
-    facet normal 0 0 1
-        outer loop
-        vertex 0 0 0
-        vertex 0 1 0
-        vertex 1 1 0
-        endloop
-    endfacet
-    facet normal 0 0 1
-        outer loop
-        vertex 0 0 0
-        vertex 1 1 0
-        vertex 1 0 0
-        endloop
-    endfacet
-    facet normal 1 0 0
-        outer loop
-        vertex 0 0 0
-        vertex 0 0 1
-        vertex 0 1 1
-        endloop
-    endfacet
-    facet normal 1 0 0
-        outer loop
-        vertex 0 0 0
-        vertex 0 1 1
-        vertex 0 1 0
-        endloop
-    endfacet
-    facet normal 0 1 0
-        outer loop
-        vertex 0 0 0
-        vertex 1 0 0
-        vertex 1 0 1
-        endloop
-    endfacet
-    facet normal 0 1 0
-        outer loop
-        vertex 0 0 0
-        vertex 1 0 1
-        vertex 0 0 1
-        endloop
-    endfacet
-    facet normal 0 0 1
-        outer loop
-        vertex 0 0 1
-        vertex 1 0 1
-        vertex 1 1 1
-        endloop
-    endfacet
-    facet normal 0 0 -1
-        outer loop
-        vertex 0 0 1
-        vertex 1 1 1
-        vertex 0 1 1
-        endloop
-    endfacet
-    facet normal -1 0 0
-        outer loop
-        vertex 1 0 0
-        vertex 1 1 0
-        vertex 1 1 1
-        endloop
-    endfacet
-    facet normal -1 0 0
-        outer loop
-        vertex 1 0 0
-        vertex 1 1 1
-        vertex 1 0 1
-        endloop
-    endfacet
-    facet normal 0 -1 0
-        outer loop
-        vertex 0 1 0
-        vertex 0 1 1
-        vertex 1 1 1
-        endloop
-    endfacet
-    facet normal 0 -1 0
-        outer loop
-        vertex 0 1 0
-        vertex 1 1 1
-        vertex 1 1 0
-        endloop
-    endfacet
-    endsolid cube"""
-
-
-f = tempfile.NamedTemporaryFile(mode="w", delete=False)
-f.write(cube_stl)
-f.close()
 
 
 def flip_normals(stl_fname):
@@ -108,21 +15,14 @@ def flip_normals(stl_fname):
     reader.file_name = stl_fname
     reader.update()
 
-    x, y, z = [], [], []
     ordering = []
     polydata = reader.output
-    n = len(polydata.polys.data)
-    i = 0
-    while(i < n):
-        ordering.append([int(polydata.polys.data[i+1]),
-                         int(polydata.polys.data[i+2]),
-                         int(polydata.polys.data[i+3])])
-        i = i + 4
+    points = polydata.points.to_array()
+    x, y, z = points.T
 
-    for i in range(len(polydata.points)):
-        x.append(polydata.points[i][0])
-        y.append(polydata.points[i][1])
-        z.append(polydata.points[i][2])
+    data = polydata.polys.to_array()
+    data.shape = data.size//4,4
+    ordering = np.delete(data, 0, 1)
 
     # getting normal data of the stl file
     normals = tvtk.PolyDataNormals()
@@ -131,12 +31,8 @@ def flip_normals(stl_fname):
     normals.compute_cell_normals = 1
     normals.update()
     polydata_normals = normals.output
-    m = len(polydata_normals.cell_data.normals)
-    u, v, w = [], [], []
-    for i in range(m):
-        u.append(polydata_normals.cell_data.normals[i][0])
-        v.append(polydata_normals.cell_data.normals[i][1])
-        w.append(polydata_normals.cell_data.normals[i][2])
+
+    u, v, w = polydata_normals.cell_data.normals.to_array().T
     fig = mlab.figure(bgcolor=(0, 0, 0))
 
     # renders the given stl file
@@ -145,15 +41,8 @@ def flip_normals(stl_fname):
             )
 
     # coordinates of centroids of the trianlges
-    centroids_x, centroids_y, centroids_z = [], [], []
-    for i in range(np.shape(ordering)[0]):
-        centroid = (np.array(polydata.points[ordering[i][0]]) +
-                    np.array(polydata.points[ordering[i][1]]) +
-                    np.array(polydata.points[ordering[i][2]])) / 3
-
-        centroids_x.append(centroid[0])
-        centroids_y.append(centroid[1])
-        centroids_z.append(centroid[2])
+    centroids = np.sum(points[ordering], axis =1)/3
+    centroids_x, centroids_y, centroids_z = centroids.T
 
     # renders normals at centroids
     normals = mlab.quiver3d(centroids_x, centroids_y, centroids_z,
@@ -184,7 +73,7 @@ def flip_normals(stl_fname):
                     centroids_z[centroid_id]
                 u[centroid_id], v[centroid_id], w[centroid_id] = \
                     -1*u[centroid_id], -1*v[centroid_id], -1*w[centroid_id]
-                normals.mlab_source.reset(u=u, v=v, w=w)
+                normals.mlab_source.set(u=u, v=v, w=w)
                 outline.bounds = (x-outline_size, x+outline_size,
                                   y-outline_size, y+outline_size,
                                   z-outline_size, z+outline_size)
@@ -195,7 +84,8 @@ def flip_normals(stl_fname):
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        stl_fname = f.name
+        stl_fname = "../data/cube.stl"
     else:
         stl_fname = sys.argv[1]
     flip_normals(stl_fname)
+    mlab.show()
