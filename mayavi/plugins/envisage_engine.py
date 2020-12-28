eb75e9b4521d58d3e78e97ed72d4551e20935465
Aaron Ayres <aayres@enthought.com>
2020-12-28 14:30:55 -0700

    typo

--- mayavi/plugins/envisage_engine.py
+++ mayavi/plugins/envisage_engine.py
@@ -14,7 +14,7 @@ from tvtk.plugins.scene.i_scene_manager import \
 from tvtk.plugins.scene.ui.actions import NewScene
 from tvtk.plugins.scene import scene_editor
 from pyface.api import GUI
-from envisgae.ui.tasks.api import TaskWindow
+from envisage.ui.tasks.api import TaskWindow
 from apptools.scripting.api import recordable
 
 # Local imports.
