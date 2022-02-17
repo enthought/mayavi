f7a58345eb9315b40b7e827a5a51cd5e2efe52a2
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2022-02-17 23:10:44 +0530

    Do not generate docs on build.
    The process of building the docs requires a UI and on a Linux machine
    this will require an X-server.  The docs are not packaced in the wheel
    so this is entirely pointless and worse, this prevents installation of
    Mayavi on a headless server.

--- setup.py
+++ setup.py
@@ -267,16 +267,6 @@ class MyBuild(build.build):
     def run(self):
         build_tvtk_classes_zip()
         build.build.run(self)
-        try:
-            self.run_command('gen_docs')
-        except:
-            log.warn("Couldn't generate documentation:\n%s" %
-                     traceback.format_exception(*sys.exc_info()))
-        try:
-            self.run_command('build_docs')
-        except:
-            log.warn("Couldn't build documentation:\n%s" %
-                     traceback.format_exception(*sys.exc_info()))
 
 
 class MyDevelop(develop.develop):
