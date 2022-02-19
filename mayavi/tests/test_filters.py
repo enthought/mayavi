bb000b93d5a947f0ec405cd7859141cf2d618051
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2022-02-19 08:29:11 +0530

    Use get_example_data instead of relpath to data.

--- mayavi/tests/test_filters.py
+++ mayavi/tests/test_filters.py
@@ -1,8 +1,8 @@
 import unittest
 from unittest.mock import patch
-import os
 
 from mayavi import mlab
+from .common import get_example_data
 from .test_engine_manager import patch_backend
 
 
@@ -14,7 +14,7 @@ class TestFilters(unittest.TestCase):
     @patch_pyface()
     @patch_backend('test')
     def test_cell_to_point_data(self):
-        src = mlab.pipeline.open(os.path.join('data', 'pyramid_ug.vtu'))
+        src = mlab.pipeline.open(get_example_data('pyramid_ug.vtu'))
         cd = mlab.pipeline.cell_derivatives(src)
         cd.filter.vector_mode = 'compute_vorticity'
         c2p = mlab.pipeline.cell_to_point_data(cd)
