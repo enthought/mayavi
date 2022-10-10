308eee049bf1ca5991fc643ef35c1e4390ae885f
Eric Larson <larson.eric.d@gmail.com>
2022-10-10 17:43:08 -0400

    FIX: One more

--- tvtk/conftest.py
+++ tvtk/conftest.py
@@ -4,10 +4,11 @@
 def pytest_report_header(config, start_path, startdir):
     from tvtk.pyface.api import DecoratedScene  # noqa
     from traits.etsconfig.api import ETSConfig
+    import pyface
     infos = list()
     infos.append(f'ETSConfig.toolkit={repr(ETSConfig.toolkit)}')
+    infos.append(f'pyface={repr(pyface.__version__)}')
     if ETSConfig.toolkit in ('qt4', 'qt'):
-        import pyface
         from pyface.qt import api_name
         infos.append(f'api_name={repr(api_name)}')
     return ', '.join(infos)
