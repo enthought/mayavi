20046ef1f9bcabcb604a06b8502e84e65c9794e4
Eric Larson <larson.eric.d@gmail.com>
2024-02-08 17:50:12 -0500

    FIX: Not extra

--- tvtk/wrapper_gen.py
+++ tvtk/wrapper_gen.py
@@ -680,11 +680,13 @@ class WrapperGenerator:
                    and (klass.__name__ == 'vtkRenderWindow') \
                    and (m == 'StereoType'):
                 extra_val = 0
-            elif (klass.__name__ in ("CubeAxesActor", "PolarAxesActor") and \
+            elif (klass.__name__ in ("vtkCubeAxesActor", "vtkPolarAxesActor") and \
                   m == "enable_distance_lod"):
                 extra_val = True
-            elif klass.__name__ == "AxisActor" and m == "axis_on_origin":
+                vtk_val = 0
+            elif klass.__name__ == "vtkAxisActor" and m == "axis_on_origin":
                 extra_val = False
+                vtk_val = 0
 
             if not vtk_val:
                 default = self._reform_name(meths[m][0][0])
